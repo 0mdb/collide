9e4f89c9fc6d7f235789b2802942f36a51764994
phaedrat <phaedratarrant@gmail.com>
2022-11-16 20:23:42 -0700

    Fixing constraints and comments

--- backend/app/app/database/schema_creation/alchemy_build.py
+++ backend/app/app/database/schema_creation/alchemy_build.py
@@ -24,6 +24,7 @@ if drop_tables:
     #metadata.reflect(sa_engine, schema='lf_mockup')
 else:
 
+    # Information source/Reference
     source = sa.Table('source',
                       metadata,
                       sa.Column('id', sa.BigInteger, sa.Identity(), primary_key=True),
@@ -32,41 +33,45 @@ else:
                       sa.Column('misc_data', pg.JSONB),
                       )
 
-
+    # Individual humans
     person = sa.Table('person',
                       metadata,
                       sa.Column('id', sa.BigInteger, sa.Identity(), primary_key=True),
-                      sa.Column('display_name', sa.Text, unique=True),
-                      sa.Column('match_name', sa.Text, unique=True),
-                      sa.Column('source', sa.BigInteger, sa.ForeignKey('source.id'))
+                      sa.Column('display_name', sa.Text, unique=True, nullable=False),
+                      sa.Column('match_name', sa.Text, unique=True, nullable=False),
+                      sa.Column('source', sa.BigInteger, sa.ForeignKey('source.id'), nullable=False)
                       )
 
+    # Standard non-overlapping sector/topic
     sector = sa.Table('sector',
                       metadata,
                       sa.Column('id', sa.Integer, sa.Identity(), primary_key=True),
-                      sa.Column('display_name', sa.Text, unique=True),
-                      sa.Column('match_name', sa.Text, unique=True),
+                      sa.Column('display_name', sa.Text, unique=True, nullable=False),
+                      sa.Column('match_name', sa.Text, unique=True, nullable=False),
                       )
 
+    # Corporations, government, lobbying groups, special interests, etc.
     org_type = sa.Table('organization_type',
                         metadata,
                         sa.Column('id', sa.Integer, sa.Identity(), primary_key=True),
-                        sa.Column('display_name', sa.Text, unique=True),
-                        sa.Column('match_name', sa.Text, unique=True),
+                        sa.Column('display_name', sa.Text, unique=True, nullable=False),
+                        sa.Column('match_name', sa.Text, unique=True, nullable=False),
                         )
 
+    # Organizations (corporations, government, cabinet, associations, etc.)
     organization = sa.Table('organization',
                             metadata,
                             sa.Column('id', sa.BigInteger, sa.Identity(), primary_key=True),
-                            sa.Column('display_name', sa.Text, unique=True),
-                            sa.Column('match_name', sa.Text, unique=True),
+                            sa.Column('display_name', sa.Text, unique=True, nullable=False),
+                            sa.Column('match_name', sa.Text, unique=True, nullable=False),
                             sa.Column('organization_type', sa.Integer, sa.ForeignKey('organization_type.id'), nullable=False),
                             sa.Column('parent_organization', sa.BigInteger, sa.ForeignKey('organization.id')),
-                            sa.Column('sector', sa.Integer, sa.ForeignKey('sector.id')),
+                            sa.Column('sector', sa.Integer, sa.ForeignKey('sector.id'), nullable=False),
                             sa.Column('source', sa.BigInteger, sa.ForeignKey('source.id'), nullable=False),
                             sa.Column('misc_data', pg.JSONB)
                             )
 
+    # People belonging to organizations
     organization_membership = sa.Table('organization_membership',
                                        metadata,
                                        sa.Column('id', sa.BigInteger, sa.Identity(), primary_key=True),
@@ -78,33 +83,37 @@ else:
                                        sa.Column('end_date', sa.Date)
                                        )
 
+    # Person to Person (comms log)
     communications = sa.Table('communications',
                               metadata,
                               sa.Column('id', sa.BigInteger, sa.Identity(), primary_key=True),
                               sa.Column('party_1', sa.BigInteger, sa.ForeignKey('person.id'), nullable=False),
                               sa.Column('party_2', sa.BigInteger, sa.ForeignKey('person.id'), nullable=False),
-                              sa.Column('date', sa.Date),
+                              sa.Column('date', sa.Date, nullable=False),
                               sa.Column('sector', sa.Integer, sa.ForeignKey('sector.id')),
                               sa.Column('source', sa.BigInteger, sa.ForeignKey('source.id'), nullable=False)
                               )
 
+    # Monetary exchange (future use)
     funding = sa.Table('funding',
                        metadata,
                        sa.Column('id', sa.BigInteger, sa.Identity(), primary_key=True),
                        sa.Column('party_1', sa.BigInteger, sa.ForeignKey('organization.id'), nullable=False),
                        sa.Column('party_2', sa.BigInteger, sa.ForeignKey('organization.id'), nullable=False),
+                       sa.Column('amount', sa.Integer, nullable=False),
                        sa.Column('start_date', sa.Date),
                        sa.Column('end_date', sa.Date),
                        sa.Column('source', sa.BigInteger, sa.ForeignKey('source.id'), nullable=False)
                        )
 
+    # Details of the corporation (an organization of type corporation)
     corporation_info = sa.Table('corporate_info',
                                 metadata,
                                 sa.Column('id', sa.BigInteger, sa.Identity(), primary_key=True),
-                                sa.Column('organization', sa.BigInteger, sa.ForeignKey('organization.id'), nullable=False),
-                                sa.Column('corporate_number', sa.BigInteger),
+                                sa.Column('organization', sa.BigInteger, sa.ForeignKey('organization.id'), nullable=False, unique=True),
+                                sa.Column('corporate_number', sa.BigInteger, nullable=False, unique=True),
+                                sa.Column('stock_ticker', sa.String(10), unique=True),
                                 sa.Column('source', sa.BigInteger, sa.ForeignKey('source.id'), nullable=False))
-
 
     metadata.create_all(sa_engine)
 
