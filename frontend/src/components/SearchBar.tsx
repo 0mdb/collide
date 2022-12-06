24cfa04f1a19ebf88fe09f3b066cfbe8966ec2df
Ben Welwood <benwelwood@gmail.com>
2022-12-06 16:47:51 -0700

    add search bar

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { InputGroup } from '@blueprintjs/core'
+
+function SearchBar() {
+  return <InputGroup className='search-bar' type='search' />
+}
+
+export default SearchBar
