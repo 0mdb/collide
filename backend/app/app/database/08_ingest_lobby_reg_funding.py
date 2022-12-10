4e6618cd9481d810bbaef0a2ff8e4277f5d56899
phaedrat <phaedratarrant@gmail.com>
2022-12-09 20:54:44 -0700

    Notes on data quality

---
+++ backend/app/app/database/08_ingest_lobby_reg_funding.py
@@ -0,0 +1,5 @@
+# TODO: Write ingestion of lobby_regs/Registration_GovtFundingExport.csv
+# Note - "Institution" also includes description of program/cause in older entries
+# Prob remove anything post "$"; Also remove anything "(x)"
+# Also there are "null", "not applicable", "municipal", "provincial", "Other provinces", "foreign", "other federal",
+# "provincial grants", "miscellaneous federal grans", "miscellaneous federal grants" and other useless things
