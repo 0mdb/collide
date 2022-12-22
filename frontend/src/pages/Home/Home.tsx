a74b27767a4ab352c1784d13e8598dbfa48533cb
Ben Welwood <benwelwood@gmail.com>
2022-12-22 16:48:23 -0700

    Add searchbar (absolute css results in no click)

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,19 +1,78 @@
-import { Colors } from '@blueprintjs/core'
 import React from 'react'
 import { ForceGraph2D } from 'react-force-graph'
-import myData from './sample_graph3.json'
+import { FormGroup, InputGroup, MenuItem, Classes } from '@blueprintjs/core'
+import { Suggest2 } from '@blueprintjs/select'
+import myData from './sample_graph4.json'
 
-const ForceGraph3Dprops = {
-  graphData: myData,
+// searchbar for nodes in myData that uses Suggest2 for names and stores filtered results in state 
+function SearchBar() {
+  const [filteredNodes, setFilteredNodes] = React.useState([])
+  const [query, setQuery] = React.useState('')
+  const [selected, setSelected] = React.useState(null)
+  const [node, setNode] = React.useState(null)
+
+  const handleQueryChange = (query) => {
+    setQuery(query)
+    setFilteredNodes(
+      myData.nodes.filter((node) => node.name.toLowerCase().includes(query.toLowerCase()))
+    )
+  }
+
+  const handleNodeSelect = (node) => {
+    setNode(node)
+    setQuery('')
+  }
+
+  const renderNode = (node, { handleClick, modifiers }) => {
+    if (!modifiers.matchesPredicate) {
+      return null
+    }
+    return (
+      <MenuItem
+        active={modifiers.active}
+        disabled={modifiers.disabled}
+        key={node.id}
+        onClick={handleClick}
+        text={node.name}
+      />
+    )
+  }
+
+  return (
+      <FormGroup
+        labelFor='text-input'
+        className='searchbar'
+      >
+        <Suggest2
+          inputValueRenderer={(node) => node.name}
+          items={filteredNodes}
+          itemRenderer={renderNode}
+          noResults={<MenuItem disabled={true} text='No results.' />}
+          onItemSelect={handleNodeSelect}
+          onQueryChange={handleQueryChange}
+          query={query}
+          resetOnSelect={true}
+        >
+          <InputGroup
+            leftIcon='search'
+            type='search'
+            placeholder='Search for nodes...'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </Suggest2>
+      </FormGroup>
+  )
 }
+
 
 function Home() {
   return (
     <div className='Home'>
+      <SearchBar />
       <ForceGraph2D
         graphData={myData}
         nodeAutoColorBy='id'
-        linkColor={Colors.GOLD3}
         linkDirectionalParticles='value'
         linkCurvature='curvature'
         linkDirectionalParticles={1}
@@ -21,5 +80,6 @@ function Home() {
     </div>
   )
 }
+
 
 export default Home
