b70147f585fda0631c482525df10ec1deb0f641d
Ben Welwood <benwelwood@gmail.com>
2022-12-22 16:49:00 -0700

    try and fail at lazy loading multiple graphs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import jsonServerProvider from 'ra-data-json-server'
-import { Routes, Route } from 'react-router-dom'
+import React, { lazy, Suspense } from 'react'
+import jsonserverprovider from 'ra-data-json-server'
+import { Route, Routes } from 'react-router-dom'
 import { H1 } from '@blueprintjs/core'
 import { Admin, Resource, ListGuesser } from 'react-admin'
 import PersistLogin from './components/PersistLogin'
 import './App.scss'
 import Home from './pages/Home'
 import Layout from './pages/Layout'
-import Graph2 from './pages/Graph2'
-import Graph1 from './pages/Graph1'
+// import Graph2 from './pages/Graph2'
+// import Graph1 from './pages/Graph1'
 import Login from './pages/Login'
 import Unauthorized from './components/Unauthorized'
 import RequireAuth from './components/RequireAuth'
 import Register from './pages/Register'
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com')
+const dataProvider = jsonserverprovider('http://jsonplaceholder.typicode.com')
+
+// const Home = lazy(() => import('./pages/Home'))
+// const Graph1 = lazy(() => import('./pages/Graph1'))
+// const Graph2 = lazy(() => import('./pages/Graph2'))
+// const Login = lazy(() => import('./pages/Login'))
+// const Register = lazy(() => import('./pages/Register'))
+// const Unauthorized = lazy(() => import('./components/Unauthorized'))
+// const RequireAuth = lazy(() => import('./components/RequireAuth'))
+
 
 const ROLES = {
   User: 'user',
@@ -24,20 +33,17 @@ const ROLES = {
 function App() {
   return (
     <Routes>
+      {/* <Suspense fallback={<H1>Loading...</H1>}> */}
       <Route path='/' element={<Layout />}>
         {/* public routes */}
         <Route path='login' element={<Login />} />
         <Route path='register' element={<Register />} />
         <Route path='/' element={<Home />} />
         <Route path='unauthorized' element={<Unauthorized />} />
+        {/* <Route path='graph1' element={<Graph1 />} /> */}
+        {/* <Route path='graph2' element={<Graph2 />} /> */}
         {/* protected routes */}
         <Route element={<PersistLogin />}>
-          <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-            <Route path='graph1' element={<Graph1 />} />
-          </Route>
-          <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-            <Route path='graph2' element={<Graph2 />} />
-          </Route>
           <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
             <Route
               path='admin/*'
@@ -52,6 +58,7 @@ function App() {
         {/* catch all */}
         <Route path='*' element={<H1>Page Missing</H1>} />
       </Route>
+      {/* </Suspense> */}
     </Routes>
   )
 }
