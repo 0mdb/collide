7ce34cad550fd3ed72d85c9383cbfac0a9440cef
Ben Welwood <benwelwood@gmail.com>
2022-12-24 21:01:18 -0700

    remove unused files

--- frontend/src/components/SearchBar.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { InputGroup } from '@blueprintjs/core'
-
-function SearchBar() {
-  return <InputGroup className='search-bar' type='search' />
-}
-
-export default SearchBar
