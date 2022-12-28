bb508f476a62f744d14fcf36fd7676db000fb59e
jrodway <jrodway@ualberta.ca>
2022-12-27 21:48:15 -0700

    adding other types of links that need to be updated upon removing a duplicated person

--- backend/app/app/database/fix_people_001.py
+++ backend/app/app/database/fix_people_001.py
@@ -1,18 +1,27 @@
-from schema_creation.sqlmodel_build import Person, OrganizationMembership, Communications
+from schema_creation.sqlmodel_build import (
+    Person,
+    OrganizationMembership,
+    Communications,
+    CommunicationsPersonOrg,
+    FundingPersonPerson,
+    FundingPersonOrg,
+)
 from sqlmodel import create_engine, select, Session, col, or_
 
 from parse_injest.utils import create_match_name
 
 from fix_people_000_common import shit_list_combined
 
-db_host = "localhost"
-db_name = "lq_test"
+db_host = "192.168.0.10"
+db_name = "collide"
 db_user = "test_user"
 db_pw = "change_this"
 
 schema_name = "lf_mockup_2"
 
-engine = create_engine(f"postgresql+psycopg2://{db_user}:{db_pw}@{db_host}/{db_name}", echo=False)
+engine = create_engine(
+    f"postgresql+psycopg2://{db_user}:{db_pw}@{db_host}/{db_name}", echo=False
+)
 sess = Session(engine)
 
 sql_query = select(Person)
@@ -28,9 +37,9 @@ for p in res:
     if len(loc_res) > 1:  # we have more than one name to deal with
         # redo the current match_name
         nt = p.display_name.split(" ")
-        nt = [x[:-1] if x.endswith(',') else x for x in nt]
+        nt = [x[:-1] if x.endswith(",") else x for x in nt]
         nt = [x for x in nt if x.lower() not in shit_list_combined]
-        redone_match_name = create_match_name(' '.join(nt))
+        redone_match_name = create_match_name(" ".join(nt))
 
         name_lengths = {}
 
@@ -41,9 +50,13 @@ for p in res:
             print(f"\t{pp.display_name} (id: {pp.id})")
 
             name_tokens = pp.display_name.split(" ")  # split on spaces
-            name_tokens = [x[:-1] if x.endswith(',') else x for x in name_tokens]  # strip trailing commas
+            name_tokens = [
+                x[:-1] if x.endswith(",") else x for x in name_tokens
+            ]  # strip trailing commas
 
-            name_tokens = [x for x in name_tokens if x.lower() not in shit_list_combined]
+            name_tokens = [
+                x for x in name_tokens if x.lower() not in shit_list_combined
+            ]
             new_match_name = create_match_name(" ".join(name_tokens))
             if new_match_name == redone_match_name:
                 print("\tmarked equivalent")
@@ -52,7 +65,7 @@ for p in res:
                     f.append(pp)
 
         if len(name_lengths.keys()) > 1:
-            print('\t\tsome redundancies to remove')
+            print("\t\tsome redundancies to remove")
             srted = dict(sorted(name_lengths.items(), key=lambda item: item[1][0]))
 
             id_to_keep = list(srted.keys())[0]
@@ -63,7 +76,9 @@ for p in res:
             for rem in id_list_to_remove:
                 print(f"\t\t\tremoval of {srted[rem][1]} (id: {rem})")
 
-                sql_query = select(OrganizationMembership).where(OrganizationMembership.person == rem)
+                sql_query = select(OrganizationMembership).where(
+                    OrganizationMembership.person == rem
+                )
                 memberships_to_update = sess.exec(sql_query).all()
                 if len(memberships_to_update) > 0:
                     print("\t\t\t\tthe following memberships should be updated:")
@@ -73,7 +88,9 @@ for p in res:
                         sess.add(mem)
                     sess.commit()
 
-                sql_query = select(Communications).where(or_(Communications.party_1 == rem, Communications.party_2 == rem))
+                sql_query = select(Communications).where(
+                    or_(Communications.party_1 == rem, Communications.party_2 == rem)
+                )
                 communications_to_update = sess.exec(sql_query).all()
                 if len(communications_to_update) > 0:
                     print("\t\t\t\tthe following communications should be updated:")
@@ -87,6 +104,43 @@ for p in res:
 
                     sess.commit()
 
-                finally_delete = sess.exec(select(Person).where(Person.id == rem)).first()
+                sql_query = select(CommunicationsPersonOrg).where(CommunicationsPersonOrg.person == rem)
+                communications_to_update = sess.exec(sql_query).all()
+                if len(communications_to_update) > 0:
+                    print("\t\t\t\tthe following communications should be updated:")
+                    for com in communications_to_update:
+                        print(f"\t\t\t\t\t{com}")
+                        com.person = id_to_keep
+                        sess.add(com)
+                    sess.commit()
+
+                sql_query = select(FundingPersonPerson).where(or_(FundingPersonPerson.party_1 == rem, FundingPersonPerson.party_2 == rem))
+                fundings_to_update = sess.exec(sql_query).all()
+                if len(fundings_to_update) > 0:
+                    print("\t\t\t\tthe following funding relationships should be updated:")
+                    for fun in fundings_to_update:
+                        print(f"\t\t\t\t\t{fun}")
+                        if fun.party_1 == rem:
+                            fun.party_1 = id_to_keep
+                        elif fun.party_2 == rem:
+                            fun.party_2 = id_to_keep
+                        sess.add(fun)
+                    sess.commit()
+
+                sql_query = select(FundingPersonOrg).where(FundingPersonOrg.person == rem)
+                fundings_to_update = sess.exec(sql_query).all()
+                if len(fundings_to_update) > 0:
+                    print("\t\t\t\tthe following funding relationships should be updated:")
+                    for fun in fundings_to_update:
+                        print(f"\t\t\t\t\t{fun}")
+                        fun.person = id_to_keep
+                        sess.add(fun)
+                    sess.commit()
+
+                finally_delete = sess.exec(
+                    select(Person).where(Person.id == rem)
+                ).first()
                 sess.delete(finally_delete)
                 sess.commit()
+
+sess.close()
