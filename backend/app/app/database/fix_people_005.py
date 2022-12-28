58a7975f47ffab560b35378679d138e4db271203
jrodway <jrodway@ualberta.ca>
2022-12-27 22:05:17 -0700

    updating with other tables to check

--- backend/app/app/database/fix_people_005.py
+++ backend/app/app/database/fix_people_005.py
@@ -1,4 +1,11 @@
-from schema_creation.sqlmodel_build import Person, OrganizationMembership, Communications
+from schema_creation.sqlmodel_build import (
+    Person,
+    OrganizationMembership,
+    Communications,
+    CommunicationsPersonOrg,
+    FundingPersonPerson,
+    FundingPersonOrg,
+)
 from sqlmodel import create_engine, select, Session, col, or_
 
 from parse_injest.utils import create_match_name
@@ -10,7 +17,9 @@ db_pw = "change_this"
 
 schema_name = "lf_mockup_2"
 
-engine = create_engine(f"postgresql+psycopg2://{db_user}:{db_pw}@{db_host}/{db_name}", echo=False)
+engine = create_engine(
+    f"postgresql+psycopg2://{db_user}:{db_pw}@{db_host}/{db_name}", echo=False
+)
 sess = Session(engine)
 
 all_people = sess.exec(select(Person)).all()
@@ -38,17 +47,88 @@ for person in all_people:
     res = sess.exec(sql_query).first()
 
     if res is not None:
-        forward_mem = sess.query(OrganizationMembership).where(OrganizationMembership.person == person.id).count()
-        forward_com = sess.query(Communications).where(or_(Communications.party_1 == person.id, Communications.party_2 == person.id)).count()
-        backward_mem = sess.query(OrganizationMembership).where(OrganizationMembership.person == res.id).count()
-        backward_com = sess.query(Communications).where(or_(Communications.party_1 == res.id, Communications.party_2 == res.id)).count()
+        forward_mem = (
+            sess.query(OrganizationMembership)
+            .where(OrganizationMembership.person == person.id)
+            .count()
+        )
+        forward_com = (
+            sess.query(Communications)
+            .where(
+                or_(
+                    Communications.party_1 == person.id,
+                    Communications.party_2 == person.id,
+                )
+            )
+            .count()
+        )
+        forward_com += (
+            sess.query(CommunicationsPersonOrg)
+            .where(CommunicationsPersonOrg.person == person.id)
+            .count()
+        )
+
+        forward_fun = (
+            sess.query(FundingPersonOrg)
+            .where(FundingPersonOrg.person == person.id)
+            .count()
+        )
+        forward_fun += (
+            sess.query(FundingPersonPerson)
+            .where(
+                or_(
+                    FundingPersonPerson.party_1 == person.id,
+                    FundingPersonPerson.party_2 == person.id,
+                )
+            )
+            .count()
+        )
+
+        backward_mem = (
+            sess.query(OrganizationMembership)
+            .where(OrganizationMembership.person == res.id)
+            .count()
+        )
+        backward_com = (
+            sess.query(Communications)
+            .where(
+                or_(Communications.party_1 == res.id, Communications.party_2 == res.id)
+            )
+            .count()
+        )
+        backward_com += (
+            sess.query(CommunicationsPersonOrg)
+            .where(CommunicationsPersonOrg.person == res.id)
+            .count()
+        )
+
+        backward_fun = (
+            sess.query(FundingPersonOrg)
+            .where(FundingPersonOrg.person == res.id)
+            .count()
+        )
+        backward_fun += (
+            sess.query(FundingPersonPerson)
+            .where(
+                or_(
+                    FundingPersonPerson.party_1 == res.id,
+                    FundingPersonPerson.party_2 == res.id,
+                )
+            )
+            .count()
+        )
+
         print(f"{dn} is also in the database as {reversed_dn}")
-        print(f"\tforward mem {forward_mem}, forward com {forward_com}")
-        print(f"\tbackward mem {backward_mem}, backward com {backward_com}")
+        print(
+            f"\tforward mem {forward_mem}, forward com {forward_com}, forward fun {forward_fun}"
+        )
+        print(
+            f"\tbackward mem {backward_mem}, backward com {backward_com}, backward fun {backward_fun}"
+        )
 
         # take the version with the most combined combinations and memberships to keep, and remove the other one
-        comb_forward = forward_mem + forward_com
-        comb_backward = backward_mem + backward_com
+        comb_forward = forward_mem + forward_com + forward_fun
+        comb_backward = backward_mem + backward_com + backward_fun
 
         if comb_forward >= comb_backward:
             print(f"\tkeeping {dn}, getting rid of {reversed_dn}")
@@ -59,7 +139,9 @@ for person in all_people:
             id_to_keep = res.id
             id_to_axe = person.id
 
-        sql_query = select(OrganizationMembership).where(OrganizationMembership.person == id_to_axe)
+        sql_query = select(OrganizationMembership).where(
+            OrganizationMembership.person == id_to_axe
+        )
         memberships_to_update = sess.exec(sql_query).all()
         if len(memberships_to_update) > 0:
             print("\t\tthe following memberships should be updated:")
@@ -70,7 +152,10 @@ for person in all_people:
             sess.commit()
 
         sql_query = select(Communications).where(
-            or_(Communications.party_1 == id_to_axe, Communications.party_2 == id_to_axe))
+            or_(
+                Communications.party_1 == id_to_axe, Communications.party_2 == id_to_axe
+            )
+        )
         communications_to_update = sess.exec(sql_query).all()
         if len(communications_to_update) > 0:
             print("\t\tthe following communications should be updated:")
@@ -81,6 +166,46 @@ for person in all_people:
                 elif com.party_2 == id_to_axe:
                     com.party_2 = id_to_keep
                 sess.add(com)
+            sess.commit()
+        sql_query = select(CommunicationsPersonOrg).where(
+            CommunicationsPersonOrg.person == id_to_axe
+        )
+        communications_to_update = sess.exec(sql_query).all()
+        if len(communications_to_update) > 0:
+            print(
+                f"\t\tthe following communications should be updated (person to org):"
+            )
+            for com in communications_to_update:
+                print(f"\t\t\t{com}")
+                com.person = id_to_keep
+                sess.add(com)
+            sess.commit()
+
+        sql_query = select(FundingPersonOrg).where(FundingPersonOrg.person == id_to_axe)
+        fundings_to_update = sess.exec(sql_query).all()
+        if len(fundings_to_update) > 0:
+            print(f"\t\tthe following fundings should be updated (person-org):")
+            for fun in fundings_to_update:
+                print(f"\t\t\t{fun}")
+                fun.person = id_to_keep
+                sess.add(fun)
+            sess.commit()
+
+        sql_query = select(FundingPersonPerson).where(
+            or_(
+                FundingPersonPerson.party_1 == id_to_axe,
+                FundingPersonPerson.party_2 == id_to_axe,
+            )
+        )
+        fundings_to_update = sess.exec(sql_query).all()
+        if len(fundings_to_update) > 0:
+            print(f"\t\tthe following fundings should be updated (person-person):")
+            for fun in fundings_to_update:
+                if fun.party_1 == id_to_axe:
+                    fun.party_1 = id_to_keep
+                else:
+                    fun.party_2 = id_to_keep
+                sess.add(fun)
             sess.commit()
 
         finally_delete = sess.exec(select(Person).where(Person.id == id_to_axe)).first()
