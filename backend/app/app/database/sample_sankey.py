cc80fe2812a5395d1c6dc2064792f3ab3d6d8583
phaedrat <phaedratarrant@gmail.com>
2022-12-29 09:51:28 -0700

    Sankey json creation (FundingPersonOrg debugging)

--- backend/app/app/database/sample_sankey.py
+++ backend/app/app/database/sample_sankey.py
@@ -103,6 +103,65 @@ for each_family in family_to_people_orgs_vals.keys():
                                          "target": each_party,
                                          "value": family_to_people_orgs_vals[each_family][each_party]})
 
+# People to org to party funding
+# Grab person -> org
+family_to_riding_vals = {}
+family_to_riding_party_vals = {}
+family_to_party_vals = {}
+for each_family in fam_results_2ormore.keys():
+    family_to_riding_vals[each_family] = 0
+    family_to_riding_party_vals[each_family] = {}
+    family_to_party_vals[each_family] = {}
+
+    for each_party in party_org_ids:
+        family_to_riding_party_vals[each_family][party_org_id_display_map.get(each_party)] = 0
+        family_to_party_vals[each_family][party_org_id_display_map.get(each_party)] = 0
+
+    for each_person_id in fam_results_2ormore[each_family]:
+        stat = select(FundingPersonOrg).where(
+            FundingPersonOrg.person == each_person_id
+        ).where(
+            FundingPersonOrg.amount > 0
+        )
+        funds_from_person = session.exec(stat).all()
+
+        for each_tsn in funds_from_person:
+            recip_id = each_tsn.organization
+
+            # each_tsn directly to party
+            if recip_id in party_org_ids:
+                recip_party_id = recip_id
+                family_to_party_vals[each_family][party_org_id_display_map.get(recip_id)] = family_to_party_vals[each_family][party_org_id_display_map.get(recip_id)] + each_tsn.amount
+            # each_tsn to riding org then to party
+            else:
+                family_to_riding_vals[each_family] = family_to_riding_vals[each_family] + each_tsn.amount
+                stat = select(Organization).where(
+                    Organization.id == recip_id
+                )
+                ret = session.exec(stat).all()
+                recip_party_id = ret[0].parent_organization
+
+                display_key_party = party_org_id_display_map.get(recip_party_id)
+                family_to_riding_party_vals[each_family][display_key_party] = family_to_riding_party_vals[each_family][display_key_party] + each_tsn.amount
+
+for each_family in family_to_riding_vals.keys():
+    final_form_json["links"].append({"source": each_family,
+                                     "target": f"riding associations",
+                                     "value": family_to_riding_vals[each_family]})
+
+for each_family in family_to_riding_party_vals.keys():
+    for each_party in family_to_riding_party_vals[each_family].keys():
+        final_form_json["links"].append({"source": f"riding associations",
+                                         "target": each_party,
+                                         "value": family_to_riding_party_vals[each_family][each_party]})
+
+for each_family in family_to_party_vals.keys():
+    for each_party in family_to_party_vals[each_family].keys():
+        final_form_json["links"].append({"source": each_family,
+                                         "target": each_party,
+                                         "value": family_to_party_vals[each_family][each_party]})
+
+# Remove links with value = 0, trim nodes
 trimmed_link_list = [x for x in final_form_json["links"] if x.get("value") > 0]
 final_form_json["links"] = trimmed_link_list
 source_nodes = [{"name": x.get("source")} for x in final_form_json["links"]]
