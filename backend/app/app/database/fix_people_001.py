9dd55618c82940b3672844d7820b3b1bbdc4e3f9
jrodway <jrodway@ualberta.ca>
2023-01-01 10:52:49 -0700

    adding dry run flags

--- backend/app/app/database/fix_people_001.py
+++ backend/app/app/database/fix_people_001.py
@@ -12,6 +12,8 @@ from parse_injest.utils import create_match_name
 
 from fix_people_000_common import shit_list_combined
 
+actually_do_it = True
+
 db_host = "192.168.0.10"
 db_name = "collide"
 db_user = "test_user"
@@ -28,6 +30,7 @@ sql_query = select(Person)
 
 res = sess.exec(sql_query).all()
 
+num_people_removed = 0
 
 for p in res:
 
@@ -74,6 +77,7 @@ for p in res:
 
             id_list_to_remove = list(srted.keys())[1:]
             for rem in id_list_to_remove:
+                num_people_removed += 1
                 print(f"\t\t\tremoval of {srted[rem][1]} (id: {rem})")
 
                 sql_query = select(OrganizationMembership).where(
@@ -84,9 +88,11 @@ for p in res:
                     print("\t\t\t\tthe following memberships should be updated:")
                     for mem in memberships_to_update:
                         print(f"\t\t\t\t\t{mem}")
-                        mem.person = id_to_keep
-                        sess.add(mem)
-                    sess.commit()
+                        if actually_do_it:
+                            mem.person = id_to_keep
+                            sess.add(mem)
+                    if actually_do_it:
+                        sess.commit()
 
                 sql_query = select(Communications).where(
                     or_(Communications.party_1 == rem, Communications.party_2 == rem)
@@ -96,13 +102,15 @@ for p in res:
                     print("\t\t\t\tthe following communications should be updated:")
                     for com in communications_to_update:
                         print(f"\t\t\t\t\t{com}")
-                        if com.party_1 == rem:
-                            com.party_1 = id_to_keep
-                        elif com.party_2 == rem:
-                            com.party_2 = id_to_keep
-                        sess.add(com)
+                        if actually_do_it:
+                            if com.party_1 == rem:
+                                com.party_1 = id_to_keep
+                            elif com.party_2 == rem:
+                                com.party_2 = id_to_keep
+                            sess.add(com)
 
-                    sess.commit()
+                    if actually_do_it:
+                        sess.commit()
 
                 sql_query = select(CommunicationsPersonOrg).where(CommunicationsPersonOrg.person == rem)
                 communications_to_update = sess.exec(sql_query).all()
@@ -110,9 +118,11 @@ for p in res:
                     print("\t\t\t\tthe following communications should be updated:")
                     for com in communications_to_update:
                         print(f"\t\t\t\t\t{com}")
-                        com.person = id_to_keep
-                        sess.add(com)
-                    sess.commit()
+                        if actually_do_it:
+                            com.person = id_to_keep
+                            sess.add(com)
+                    if actually_do_it:
+                        sess.commit()
 
                 sql_query = select(FundingPersonPerson).where(or_(FundingPersonPerson.party_1 == rem, FundingPersonPerson.party_2 == rem))
                 fundings_to_update = sess.exec(sql_query).all()
@@ -120,12 +130,14 @@ for p in res:
                     print("\t\t\t\tthe following funding relationships should be updated:")
                     for fun in fundings_to_update:
                         print(f"\t\t\t\t\t{fun}")
-                        if fun.party_1 == rem:
-                            fun.party_1 = id_to_keep
-                        elif fun.party_2 == rem:
-                            fun.party_2 = id_to_keep
-                        sess.add(fun)
-                    sess.commit()
+                        if actually_do_it:
+                            if fun.party_1 == rem:
+                                fun.party_1 = id_to_keep
+                            elif fun.party_2 == rem:
+                                fun.party_2 = id_to_keep
+                            sess.add(fun)
+                    if actually_do_it:
+                        sess.commit()
 
                 sql_query = select(FundingPersonOrg).where(FundingPersonOrg.person == rem)
                 fundings_to_update = sess.exec(sql_query).all()
@@ -133,14 +145,21 @@ for p in res:
                     print("\t\t\t\tthe following funding relationships should be updated:")
                     for fun in fundings_to_update:
                         print(f"\t\t\t\t\t{fun}")
-                        fun.person = id_to_keep
-                        sess.add(fun)
+                        if actually_do_it:
+                            fun.person = id_to_keep
+                            sess.add(fun)
+                    if actually_do_it:
+                        sess.commit()
+
+                if actually_do_it:
+                    finally_delete = sess.exec(
+                        select(Person).where(Person.id == rem)
+                    ).first()
+                    sess.delete(finally_delete)
                     sess.commit()
 
-                finally_delete = sess.exec(
-                    select(Person).where(Person.id == rem)
-                ).first()
-                sess.delete(finally_delete)
-                sess.commit()
-
 sess.close()
+if not actually_do_it:
+    print(f"number of people that WOULD have been removed: {num_people_removed}")
+else:
+    print(f"number of people removed: {num_people_removed}")
