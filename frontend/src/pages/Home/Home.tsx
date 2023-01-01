929b5deda04932ac83dba64004dfb8d96fbeed6d
Ben Welwood <benwelwood@gmail.com>
2022-12-31 21:57:37 -0700

    big search bar

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -3,17 +3,32 @@ import ForceGraph from '../../components/ForceGraph'
 import TopNavigation from '../../components/NavBar'
 import GraphBar from '../../components/GraphBar'
 import SideBar from '../../components/SideBar'
+import Sankey from '../../components/SankeyGraph'
+import { Icon } from '@blueprintjs/core'
 
-function Home () {
+const PlusIcon = () => (
+  <Icon icon='search' size={22} className='dark:text-primary mx-2 text-green-500 dark:shadow-lg' />
+)
+
+const BottomBar = () => (
+  <div className='bottom-bar'>
+    <PlusIcon />
+    <input type='text' placeholder='Search...' className='bottom-bar-input' />
+  </div>
+)
+
+function Home() {
   return (
     <div className='flex'>
       <SideBar />
       <GraphBar />
       <div className='content-container'>
-      <TopNavigation />
-      <div className='content-list'>
-      <ForceGraph />
-      </div>
+        <TopNavigation />
+        <div className='inline-block grid'>
+          <Sankey />
+          <ForceGraph />
+        </div>
+        <BottomBar />
       </div>
     </div>
   )
