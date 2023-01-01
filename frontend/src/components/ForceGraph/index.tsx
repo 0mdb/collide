b46562a6acedd421aeb50f7a9fe95875982a9070
Ben Welwood <benwelwood@gmail.com>
2022-12-31 22:01:46 -0700

    working on grid for many plots on page

--- frontend/src/components/ForceGraph/index.tsx
+++ frontend/src/components/ForceGraph/index.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { ForceGraph2D } from 'react-force-graph'
 import myData from './sample_graph4.json'
 
-function ForceGraph () {
-    return (
-      <div className='force-graph'>
-      <ForceGraph2D
-        graphData={myData}
-        nodeAutoColorBy='id'
-        linkDirectionalParticles='value'
-        linkCurvature='curvature'
-      />
+function ForceGraph() {
+  return (
+    <div className='overflow-hidden rounded-lg shadow-lg'>
+      <div className='light:bg-inherit py-3 px-5 dark:bg-inherit'>
+        <ForceGraph2D
+          graphData={myData}
+          nodeAutoColorBy='id'
+          linkDirectionalParticles='value'
+          linkCurvature='curvature'
+        />
       </div>
-    )   
+    </div>
+  )
 }
 
 export default ForceGraph
