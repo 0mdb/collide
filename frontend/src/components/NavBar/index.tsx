e62d42736c33e0f514d6014dfdbbb5e54b475bc0
Ben Welwood <benwelwood@gmail.com>
2023-01-01 20:47:50 -0700

    add logout and shuffle icons

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,19 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { Icon } from '@blueprintjs/core'
+import { Link, useNavigate } from 'react-router-dom'
+import { Icon, Button, Classes } from '@blueprintjs/core'
+import { Tooltip2 } from '@blueprintjs/popover2'
 import useDarkMode from '../../hooks/useDarkMode'
+import useAuth from '../../hooks/useAuth'
+import useLogout from '../../hooks/useLogout'
 import SearchBar from '../Search'
 
 const TopNavigation = () => {
+  const navigate = useNavigate()
+  const logout = useLogout()
+  const { auth } = useAuth()
+
+  const signOut = async () => {
+    await logout()
+    navigate('/')
+  }
   return (
     <div className='top-navigation'>
       <Title />
       <ThemeIcon />
+      {/* <LogoutIcon /> */}
+      <Icon icon='log-out' size={24} className='top-navigation-icon' onClick={() => signOut()} />
       {/* <Search /> */}
-      <BellIcon />
+      {/* <BellIcon /> */}
       {/* <ThemeIcon /> */}
       {/* <SankeyGraph /> */}
-      <UserCircle />
+      {/* <UserCircle /> */}
     </div>
   )
 }
@@ -50,12 +63,25 @@ const ThemeIcon = () => {
 const Search = () => (
   <div className='search'>
     <input className='search-input' type='text' placeholder='Search...' />
-    <Icon icon='search' size={18} className='text-secondary my-auto' />
+    <Icon icon='search' size={18} className='text-secondary my-auto px-2' />
   </div>
 )
 const BellIcon = () => <Icon icon='notifications' size={24} className='top-navigation-icon' />
 const UserCircle = () => <Icon icon='user' size={24} className='top-navigation-icon' />
 const Title = () => <h5 className='title-text'>collide.io</h5>
 
+// {
+//   !auth.accessToken ? (
+//     <Link to='/login'>
+//       <Tooltip2 content='Login' placement='bottom-end'>
+//         <Button className={Classes.MINIMAL} icon='log-in' />
+//       </Tooltip2>
+//     </Link>
+//   ) : (
+//     <Tooltip2 content='Logout' placement='bottom-end'>
+//       <Button className={Classes.MINIMAL} icon='log-out' onClick={() => signOut()} />
+//     </Tooltip2>
+//   )
+// }
 // export { default as NavGroup } from './NavGroup'
 export default TopNavigation
