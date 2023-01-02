ecdba849b53471351b62c96222fc61f51e0e2401
phaedrat <phaedratarrant@gmail.com>
2023-01-01 19:11:21 -0700

    Individual Vote ingestion (debugged).

--- backend/app/app/database/common_func.py
+++ backend/app/app/database/common_func.py
@@ -592,6 +592,71 @@ def add_votes(session, vote_lst):
     return vote_obj_lst
 
 
+def add_individual_votes(session, ind_vote_lst):
+    """
+        {"parliament": parl[idx],
+        "parliament_session": parl_session[idx],
+        "vote_no": vote_no[idx],
+        "first_name": first_name[idx],
+        "last_name": last_name[idx],
+        "yes_bool": yes_bool[idx],
+        "no_bool": no_bool[idx],
+        "pair_bool": pair_bool[idx],
+        "source_id": detail_src_obj.id}
+    """
+    ind_vote_obj_lst = []
+    for jdx, each_dict in enumerate(ind_vote_lst):
+        print(f"IndVotes loop: {jdx} of {len(ind_vote_lst)}")
+        # Retrieve vote.id and person.id
+        vote_match_name = str(each_dict.get("parliament")) + "_" + str(each_dict.get("parliament_session")) + "_" + str(each_dict.get("vote_no"))
+        person_display_name = each_dict.get("first_name") + " " + each_dict.get("last_name")
+        person_match_name = create_match_name(person_display_name)
+
+        stat = select(Vote).where(
+            Vote.match_name == vote_match_name
+        )
+        vote_res = session.exec(stat).all()
+
+        if len(vote_res) == 0 or len(vote_res) > 1:
+            raise AssertionError(f"Vote does not exist or is not unique ({vote_match_name})")
+
+        stat = select(Person).where(
+            Person.match_name == person_match_name
+        )
+        person_res = session.exec(stat).all()
+
+        if len(person_res) == 0 or len(person_res) > 1:
+            raise AssertionError(f"Person does not exist or is not unique ({person_match_name})")
+
+        # Check if it already exists with same vote.id and person.id
+        stat = select(VoteIndividual).where(
+            VoteIndividual.person == person_res[0].id
+        ).where(
+            VoteIndividual.vote == vote_res[0].id
+        )
+        ind_vote_res = session.exec(stat).all()
+
+        if len(ind_vote_res) == 0:
+            # New entry
+            ot = VoteIndividual(vote=vote_res[0].id,
+                                person=person_res[0].id,
+                                is_yea=each_dict.get("yes_bool"),
+                                is_nay=each_dict.get("no_bool"),
+                                is_paired=each_dict.get("pair_bool"),
+                                source=each_dict.get("source_id"))
+
+            session.add(ot)
+            session.commit()
+            ind_vote_obj_lst.append(ot)
+        elif len(ind_vote_res) == 1:
+            # Existing entry
+            existing_vote = ind_vote_res[0]
+            ind_vote_obj_lst.append(existing_vote)
+        else:
+            raise RuntimeError("Non unique individual vote detected")
+    return ind_vote_obj_lst
+
+
 def match_organization_type(str_name):
     lc_name = str.lower(str_name)
 
