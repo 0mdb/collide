0341a3e9e7b5a3e900205ba65c5562f23d9f9b41
Ben Welwood <benwelwood@gmail.com>
2023-01-01 20:45:33 -0700

    fix focusing on password on load

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -7,111 +7,6 @@ import loginImage from './login_img.png'
 
 const LOGIN_URL = 'login/access-token'
 
-// function Login() {
-//   const { setAuth, persist, setPersist } = useAuth()
-
-//   const navigate = useNavigate()
-//   const location = useLocation()
-//   const from = location.state?.from?.pathname || '/'
-//   const userRef = useRef<HTMLHeadingElement>()
-//   const errRef = useRef()
-//   const [user, setUser] = useState('')
-//   const [pwd, setPwd] = useState('')
-//   const [errMsg, setErrMsg] = useState('')
-
-//   useEffect(() => {
-//     userRef.current.focus()
-//   }, [])
-
-//   useEffect(() => {
-//     setErrMsg('')
-//   }, [user, pwd])
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault()
-//     try {
-//       const formData = new FormData()
-//       formData.append('username', user)
-//       formData.append('password', pwd)
-//       const response = await axios.post(LOGIN_URL, formData)
-//       const accessToken = response?.data?.access_token
-//       const refreshToken = response?.data?.refresh_token
-//       setAuth({ user, accessToken, refreshToken })
-//       setUser('')
-//       setPwd('')
-//       navigate(from, { replace: true })
-//     } catch (err) {
-//       if (!err?.response) {
-//         setErrMsg('No Server Response')
-//       } else if (err.response?.status === 400) {
-//         setErrMsg('Incorrect email or password')
-//       } else {
-//         setErrMsg('Login Failed')
-//       }
-//       errRef.current.focus()
-//     }
-//   }
-
-//   const togglePersist = () => {
-//     setPersist(!persist)
-//   }
-//   useEffect(() => {
-//     localStorage.setItem('persist', persist)
-//   }, [persist])
-
-//   return (
-//     <div className='login'>
-//       <div>
-//         <form onSubmit={handleSubmit}>
-//           <h1>Collide</h1>
-//           <FormGroup label='Email' labelFor='username'>
-//             <input
-//               type='email'
-//               placeholder='Email'
-//               id='username'
-//               ref={userRef}
-//               autoComplete='off'
-//               onChange={(e) => setUser(e.target.value)}
-//               value={user}
-//               required
-//             />
-//           </FormGroup>
-//           <FormGroup label='Password' labelFor='password'>
-//             <input
-//               type='password'
-//               id='password'
-//               placeholder='Password'
-//               ref={userRef}
-//               onChange={(e) => setPwd(e.target.value)}
-//               value={pwd}
-//               required
-//             />
-//           </FormGroup>
-//           <p>
-//             <Button intent='primary' type='submit' text='Login' />
-//           </p>
-//         </form>
-//         <Checkbox
-//           checked={persist}
-//           label='Remember me'
-//           onChange={togglePersist}
-//           alignIndicator='left'
-//         />
-//         <p>
-//           Need an Account?
-//           <br />
-//           <span className='line'>
-//             <Link to='/register'>Sign up</Link>
-//           </span>
-//         </p>
-// <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live='assertive'>
-// {errMsg}
-// </p>
-//       </div>
-//     </div>
-//   )
-
-// }
 function Login() {
   const { setAuth, persist, setPersist } = useAuth()
 
@@ -176,7 +71,6 @@ function Login() {
         >
           <h2 className='py-6 text-center text-4xl font-bold'>collide.io</h2>
           <div className='flex flex-col py-2'>
-            {/* <label>Username</label> */}
             <input
               type='email'
               placeholder='Email'
@@ -190,9 +84,7 @@ function Login() {
             />
           </div>
           <div className='flex flex-col py-2'>
-            {/* <label>Password</label> */}
             <input
-              ref={userRef}
               placeholder='Password'
               id='password'
               className='rounded-lg border p-2 shadow-lg'
