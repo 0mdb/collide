874054c70fd4a1c60e9a38ce3f76e364114b3d16
jrodway <jrodway@ualberta.ca>
2023-01-03 20:27:45 -0700

    stripping whitespace out of org names; it just pisses me off

---
+++ backend/app/app/database/fix_orgs_001.py
@@ -0,0 +1,30 @@
+from sqlmodel import select
+from schema_creation.sqlmodel_build import (
+    Organization
+)
+from common_func import create_session
+
+if __name__ == "__main__":
+    sess = create_session(db_name='collide', db_host='192.168.0.10')
+
+    actually_do_it = True
+    num_changes = 0
+
+    sq = select(Organization)
+    orgs = sess.exec(sq)
+    for o in orgs:
+        dn = o.display_name
+        sdn = dn.strip()
+        if dn != sdn:
+            num_changes += 1
+            if actually_do_it:
+                o.display_name = sdn
+                sess.add(o)
+                sess.commit()
+
+    sess.close()
+    if actually_do_it:
+        w = ' were '
+    else:
+        w = " would have been "
+    print(f"number of changes that{w}made was: {num_changes}")
