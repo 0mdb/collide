1eedff77a604808db0611b725a9624cbe6448c39
Ben Welwood <benwelwood@gmail.com>
2023-01-04 16:13:23 -0700

    keep for now for ref

--- backend/app/app/models/item.py
+++ backend/app/app/models/item.py
@@ -9,9 +9,9 @@ if TYPE_CHECKING:
     from .user import User  # noqa: F401
 
 
-class Item(Base):
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("user.id"))
-    owner = relationship("User", back_populates="items")
+# class Item(Base):
+#     id = Column(Integer, primary_key=True, index=True)
+#     title = Column(String, index=True)
+#     description = Column(String, index=True)
+#     owner_id = Column(Integer, ForeignKey("user.id"))
+#     owner = relationship("User", back_populates="items")
