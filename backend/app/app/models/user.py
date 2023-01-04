5a9cc725fa9849bda8b22689fc9f0e4399414aa5
Ben Welwood <benwelwood@gmail.com>
2023-01-04 16:11:27 -0700

    migrate to fastapi-users user model

--- backend/app/app/models/user.py
+++ backend/app/app/models/user.py
@@ -1,19 +1,9 @@
-from typing import TYPE_CHECKING
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID
+from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 
-from sqlalchemy import Boolean, Column, Integer, String
-from sqlalchemy.orm import relationship
-
-from app.db.base_class import Base
-
-if TYPE_CHECKING:
-    from .item import Item  # noqa: F401
+Base: DeclarativeMeta = declarative_base()
 
 
-class User(Base):
-    id = Column(Integer, primary_key=True, index=True)
-    full_name = Column(String, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean(), default=True)
-    is_superuser = Column(Boolean(), default=False)
-    items = relationship("Item", back_populates="owner")
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    pass
+    # full_name = Column(String, index=True)
