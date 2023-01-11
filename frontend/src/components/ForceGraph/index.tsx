03941c6febcde0dff61fe254032e660bae66bd22
Ben Welwood <benwelwood@gmail.com>
2023-01-11 10:48:41 -0700

    graph working with api endpoint

--- frontend/src/components/ForceGraph/index.tsx
+++ frontend/src/components/ForceGraph/index.tsx
@@ -1,17 +1,26 @@
 import React, { useRef } from 'react'
 import { ForceGraph2D } from 'react-force-graph'
-import myData from './sample_graph3.json'
+import { useQuery } from '@tanstack/react-query'
+import { getGraph } from '../../api/graph'
+import Loading from '../Loading'
 
 import { useWindowSize } from '@react-hook/window-size'
 
 function ForceGraph() {
   const fgRef = useRef()
   const [width, height] = useWindowSize()
+  const { status, error, data } = useQuery({
+    queryKey: ['forcegraph'],
+    queryFn: getGraph,
+  })
+
+  if (status === 'loading') return <Loading />
+  if (error === 'error') return <div>Error</div>
 
   return (
     <ForceGraph2D
       ref={fgRef}
-      graphData={myData}
+      graphData={data}
       height={height - 400}
       width={width - 200}
       cooldownTicks={100}
@@ -22,4 +31,5 @@ function ForceGraph() {
     />
   )
 }
+
 export default ForceGraph
