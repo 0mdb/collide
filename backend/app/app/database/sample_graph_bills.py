ac2c4dee502e3175ed3945645794b43a115b8bf8
phaedrat <phaedratarrant@gmail.com>
2023-01-10 19:54:43 -0700

    Bill, vote, lobbyist styling edits plus minor debugging.

--- backend/app/app/database/sample_graph_bills.py
+++ backend/app/app/database/sample_graph_bills.py
@@ -1,12 +1,12 @@
 import common_func as cf
-from sqlmodel import Session, create_engine, select
+from sqlmodel import Session, create_engine, select, or_
 from schema_creation.sqlmodel_build import (
     Bill, Vote, VoteIndividual, Communications, LegStage, Person
 )
 import json
 import datetime
 
-bill_lookup = "38_1_c-22"
+bill_lookup = "40_2_c-50"
 
 session = cf.create_session(debug=False)
 stat = select(Bill).where(
@@ -46,11 +46,15 @@ nodes_links_dict = {"nodes": [],
                     "links": []}
 node_color_bill_line = 'black'
 node_color_vote = {"yea": 'green',
-                   "nay": 'red'}
+                   "nay": 'red',
+                   "paired": 'grey'}
 node_color_outsider = 'purple'
 
 stage_dates = {}
 stage_listing = []
+all_stage_listing = []
+node_id_lst = []
+
 if bill.is_house_bill:
     if bill.is_read_first_house:
         # add first house reading
@@ -61,10 +65,12 @@ if bill.is_house_bill:
             "id": hash(stage_lst),
             "legstage_id": leg_id,
             "name": leg_name,  # display name
-            "nodeColor": node_color_bill_line
+            "nodeColor": node_color_bill_line,
+            "nodeVal": 8
         })
         stage_dates[hash(stage_lst)] = bill.first_house_read_date
         stage_listing.append(hash(stage_lst))
+        all_stage_listing.append(hash(stage_lst))
 
     if bill.is_read_second_house:
         # add second house reading
@@ -75,10 +81,12 @@ if bill.is_house_bill:
             "id": hash(stage_lst),
             "legstage_id": leg_id,
             "name": leg_name,  # display name
-            "nodeColor": node_color_bill_line
+            "nodeColor": node_color_bill_line,
+            "nodeVal": 8
         })
         stage_dates[hash(stage_lst)] = bill.second_house_read_date
         stage_listing.append(hash(stage_lst))
+        all_stage_listing.append(hash(stage_lst))
 
     if bill.is_read_third_house:
         # add third house reading
@@ -89,10 +97,12 @@ if bill.is_house_bill:
             "id": hash(stage_lst),
             "legstage_id": leg_id,
             "name": leg_name,  # display name
-            "nodeColor": node_color_bill_line
+            "nodeColor": node_color_bill_line,
+            "nodeVal": 8
         })
         stage_dates[hash(stage_lst)] = bill.third_house_read_date
         stage_listing.append(hash(stage_lst))
+        all_stage_listing.append(hash(stage_lst))
 
     if bill.is_read_first_senate:
         # add first senate reading
@@ -103,10 +113,11 @@ if bill.is_house_bill:
             "id": hash(stage_lst),
             "legstage_id": leg_id,
             "name": leg_name,  # display name
-            "nodeColor": node_color_bill_line
+            "nodeColor": node_color_bill_line,
+            "nodeVal": 8
         })
         stage_dates[hash(stage_lst)] = bill.first_senate_read_date
-        stage_listing.append(hash(stage_lst))
+        all_stage_listing.append(hash(stage_lst))
 
     if bill.is_read_second_senate:
         # add second senate reading
@@ -117,10 +128,11 @@ if bill.is_house_bill:
             "id": hash(stage_lst),
             "legstage_id": leg_id,
             "name": leg_name,  # display name
-            "nodeColor": node_color_bill_line
+            "nodeColor": node_color_bill_line,
+            "nodeVal": 8
         })
         stage_dates[hash(stage_lst)] = bill.second_senate_read_date
-        stage_listing.append(hash(stage_lst))
+        all_stage_listing.append(hash(stage_lst))
 
     if bill.is_read_third_senate:
         # add third senate reading
@@ -131,10 +143,11 @@ if bill.is_house_bill:
             "id": hash(stage_lst),
             "legstage_id": leg_id,
             "name": leg_name,  # display name
-            "nodeColor": node_color_bill_line
+            "nodeColor": node_color_bill_line,
+            "nodeVal": 8
         })
         stage_dates[hash(stage_lst)] = bill.third_senate_read_date
-        stage_listing.append(hash(stage_lst))
+        all_stage_listing.append(hash(stage_lst))
 
 elif bill.is_senate_bill:
     if bill.is_read_first_senate:
@@ -146,10 +159,11 @@ elif bill.is_senate_bill:
             "id": hash(stage_lst),
             "legstage_id": leg_id,
             "name": leg_name,  # display name
-            "nodeColor": node_color_bill_line
+            "nodeColor": node_color_bill_line,
+            "nodeVal": 8
         })
         stage_dates[hash(stage_lst)] = bill.first_senate_read_date
-        stage_listing.append(hash(stage_lst))
+        all_stage_listing.append(hash(stage_lst))
 
     if bill.is_read_second_senate:
         # add second senate reading
@@ -160,10 +174,11 @@ elif bill.is_senate_bill:
             "id": hash(stage_lst),
             "legstage_id": leg_id,
             "name": leg_name,  # display name
-            "nodeColor": node_color_bill_line
+            "nodeColor": node_color_bill_line,
+            "nodeVal": 8
         })
         stage_dates[hash(stage_lst)] = bill.second_senate_read_date
-        stage_listing.append(hash(stage_lst))
+        all_stage_listing.append(hash(stage_lst))
 
     if bill.is_read_third_senate:
         # add third senate reading
@@ -174,10 +189,11 @@ elif bill.is_senate_bill:
             "id": hash(stage_lst),
             "legstage_id": leg_id,
             "name": leg_name,  # display name
-            "nodeColor": node_color_bill_line
+            "nodeColor": node_color_bill_line,
+            "nodeVal": 8
         })
         stage_dates[hash(stage_lst)] = bill.third_senate_read_date
-        stage_listing.append(hash(stage_lst))
+        all_stage_listing.append(hash(stage_lst))
 
     if bill.is_read_first_house:
         # add first house reading
@@ -188,10 +204,12 @@ elif bill.is_senate_bill:
             "id": hash(stage_lst),
             "legstage_id": leg_id,
             "name": leg_name,  # display name
-            "nodeColor": node_color_bill_line
+            "nodeColor": node_color_bill_line,
+            "nodeVal": 8
         })
         stage_dates[hash(stage_lst)] = bill.first_house_read_date
         stage_listing.append(hash(stage_lst))
+        all_stage_listing.append(hash(stage_lst))
 
     if bill.is_read_second_house:
         # add second house reading
@@ -202,10 +220,12 @@ elif bill.is_senate_bill:
             "id": hash(stage_lst),
             "legstage_id": leg_id,
             "name": leg_name,  # display name
-            "nodeColor": node_color_bill_line
+            "nodeColor": node_color_bill_line,
+            "nodeVal": 8
         })
         stage_dates[hash(stage_lst)] = bill.second_house_read_date
         stage_listing.append(hash(stage_lst))
+        all_stage_listing.append(hash(stage_lst))
 
     if bill.is_read_third_house:
         # add third house reading
@@ -216,10 +236,12 @@ elif bill.is_senate_bill:
             "id": hash(stage_lst),
             "legstage_id": leg_id,
             "name": leg_name,  # display name
-            "nodeColor": node_color_bill_line
+            "nodeColor": node_color_bill_line,
+            "nodeVal": 8
         })
         stage_dates[hash(stage_lst)] = bill.third_house_read_date
         stage_listing.append(hash(stage_lst))
+        all_stage_listing.append(hash(stage_lst))
 
 if bill.is_passed_royal_assent:
     # add royal assent
@@ -230,25 +252,29 @@ if bill.is_passed_royal_assent:
         "id": hash(stage_lst),
         "legstage_id": leg_id,
         "name": leg_name,  # display name
-        "nodeColor": node_color_bill_line
+        "nodeColor": node_color_bill_line,
+        "nodeVal": 8
     })
     stage_dates[hash(stage_lst)] = bill.royal_assent_date
-    stage_listing.append(hash(stage_lst))
+    all_stage_listing.append(hash(stage_lst))
+
+node_id_lst = node_id_lst + all_stage_listing
 
 # Build links between bill stages
-for idx, each_stage_hash in enumerate(stage_listing[:-1]):
+for idx, each_stage_hash in enumerate(all_stage_listing[:-1]):
     nodes_links_dict["links"].append({
-        "source": stage_listing[idx],
-        "target": stage_listing[idx+1],
+        "source": all_stage_listing[idx],
+        "target": all_stage_listing[idx+1],
         "linkColor": node_color_bill_line,
-        "linkDirectionalArrowLength": '{3.5}',  # 0 removes
-        "linkDirectionalArrowRelPos": '{1}'
+        "linkDirectionalArrowLength": 6,  # 0 removes
+        "linkDirectionalArrowRelPos": 0.5,
+        "linkWidth": 4
     })
 
 ########################################
 
 # query votes by bill stage
-for idx, each_stage_hash in enumerate(stage_listing):
+for each_stage_hash in stage_listing:
     # retrieve vote id when bill.id matches, date matches
     stat = select(Vote).where(
         Vote.bill == bill.id
@@ -268,8 +294,10 @@ for idx, each_stage_hash in enumerate(stage_listing):
             person_id = each_individual_vote.person
             if each_individual_vote.is_yea:
                 person_vote = "yea"
-            else:
+            elif each_individual_vote.is_nay:
                 person_vote = "nay"
+            else:
+                person_vote = "paired"
 
             stat = select(Person).where(
                 Person.id == person_id
@@ -280,25 +308,29 @@ for idx, each_stage_hash in enumerate(stage_listing):
             person_hash = hash((res[0].match_name, stage_dates[each_stage_hash]))
 
             # Nodes by person/vote date
-            nodes_links_dict["nodes"].append({
-                "id": person_hash,
-                "legstage_id": -1,
-                "name": person_name,  # display name
-                "nodeColor": node_color_vote[person_vote]
-            })
+            if person_hash not in node_id_lst:
+                nodes_links_dict["nodes"].append({
+                    "id": person_hash,
+                    "legstage_id": -1,
+                    "name": person_name,  # display name
+                    "nodeColor": node_color_vote[person_vote],
+                    "nodeVal": 2
+                })
+                node_id_lst.append(person_hash)
 
             # Links person -> vote
             nodes_links_dict["links"].append({
                 "source": person_hash,
                 "target": each_stage_hash,
                 "linkColor": node_color_vote[person_vote],
-                "linkDirectionalArrowLength": '{0}',  # 0 removes
-                "linkDirectionalArrowRelPos": '{0}'
+                "linkDirectionalArrowLength": 0,  # 0 removes
+                "linkDirectionalArrowRelPos": 0,
+                "linkWidth": 1
             })
 
             # query comms logs for communications within 30 days of vote date
             stat = select(Communications).where(
-                Communications.party_1 == person_id or Communications.party_2 == person_id
+                or_(Communications.party_1 == person_id, Communications.party_2 == person_id)
             ).where(
                 Communications.com_date <= stage_dates[each_stage_hash]
             ).where(
@@ -313,36 +345,38 @@ for idx, each_stage_hash in enumerate(stage_listing):
                 else:
                     other_person_id = each_communication.party_1
 
-                other_person_hash = hash((other_person_id, each_communication.com_date))
-
                 stat_other_party = select(Person).where(
                     Person.id == other_person_id
                 )
                 res_other_party = session.exec(stat_other_party).all()
 
-                nodes_links_dict["nodes"].append({
-                    "id": other_person_hash,
-                    "legstage_id": -1,
-                    "name": res_other_party[0].display_name,  # display name
-                    "nodeColor": node_color_outsider
-                })
+                other_person_hash = hash((res_other_party[0].match_name, each_communication.com_date))
+
+                if other_person_hash not in node_id_lst:
+                    nodes_links_dict["nodes"].append({
+                        "id": other_person_hash,
+                        "legstage_id": -1,
+                        "name": res_other_party[0].display_name,  # display name
+                        "nodeColor": node_color_outsider,
+                        "nodeVal": 2
+                    })
+                    node_id_lst.append(other_person_hash)
 
                 # Links other_person -> voting_person
                 nodes_links_dict["links"].append({
                     "source": other_person_hash,
                     "target": person_hash,
                     "linkColor": node_color_outsider,
-                    "linkDirectionalArrowLength": '{0}',  # 0 removes
-                    "linkDirectionalArrowRelPos": '{0}'
+                    "linkDirectionalArrowLength": 0,  # 0 removes
+                    "linkDirectionalArrowRelPos": 0,
+                    "linkWidth": 1
                 })
 
 print(nodes_links_dict)
 with open("testing.json", 'w') as f:
-    json.dump(nodes_links_dict, f)
+    json.dump(nodes_links_dict, f, indent=4)
 
 print("END")
-
-# TODO: debug json
 
 """
 {"nodes": [
