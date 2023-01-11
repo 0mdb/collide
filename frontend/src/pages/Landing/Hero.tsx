bf8952b425d2ccefaf6f666ac06c5f32c20a478a
Ben Welwood <benwelwood@gmail.com>
2023-01-10 20:44:37 -0700

    add hero row and fix moble layout

--- frontend/src/pages/Landing/Hero.tsx
+++ frontend/src/pages/Landing/Hero.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
   return (
-    <div className='text-white'>
+    <div>
       <div className='mx-auto mt-[-96px] flex h-screen w-full max-w-[800px] flex-col justify-center text-center'>
         <p className='p-2 font-bold text-[#00df9a]'>GROWING WITH DATA ANALYTICS</p>
         <h1 className='text-4xl font-bold sm:text-6xl md:py-6 md:text-7xl'>Grow with data.</h1>
@@ -14,9 +15,11 @@ const Hero = () => {
         <p className='text-xl font-bold text-gray-500 md:text-2xl'>
           Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.
         </p>
-        <button className='my-6 mx-auto w-[200px] rounded-md bg-[#00df9a] py-3 font-medium text-black'>
-          Get Started
-        </button>
+        <Link to='/register'>
+          <button className='my-6 mx-auto w-[200px] rounded-md bg-[#00df9a] py-3 font-medium text-black'>
+            Get Started
+          </button>
+        </Link>
       </div>
     </div>
   )
