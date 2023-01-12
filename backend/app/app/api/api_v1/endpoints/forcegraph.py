f1b1249eea28dccacd44cd74255155f774533891
Ben Welwood <benwelwood@gmail.com>
2023-01-12 16:11:16 -0700

    add place holder endpoints for new graphs

--- backend/app/app/api/api_v1/endpoints/forcegraph.py
+++ backend/app/app/api/api_v1/endpoints/forcegraph.py
@@ -4,16 +4,127 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 import json
+from app import crud, models, schemas
 
 router = APIRouter()
 
 
-@router.get("/", tags=["forcegraph"])
-def sample_graph() -> Any:
+@router.get("/sample", response_model=schemas.GraphData)
+async def sample_graph() -> Any:
     """
-    Retrieve items.
+    Sample graph data.
     """
 
-    with open("app/data/sample_graph3.json") as f:
+    with open("app/data/bloc.json") as f:
         graph = json.load(f)
         return graph
+
+
+@router.get("/bills", response_model=schemas.GraphData)
+def sample_graph() -> Any:
+    """
+    Return sample 2D BILL data.
+    """
+    sample_json = {
+        "nodes": [
+            {"id": "Myriel", "group": 1},
+            {"id": "Napoleon", "group": 1},
+            {"id": "Mlle.Baptistine", "group": 1},
+        ],
+        "links": [
+            {"source": "Napoleon", "target": "Myriel", "value": 1},
+            {"source": "Mlle.Baptistine", "target": "Myriel", "value": 8},
+        ],
+    }
+
+    return sample_json
+
+
+@router.get("/memgraph", response_model=schemas.GraphData)
+def sample_graph() -> Any:
+    """
+    Retrieve memgraph 3D data.
+    """
+    sample_json = {
+        "nodes": [
+            {"id": "Myriel", "group": 1},
+            {"id": "Napoleon", "group": 1},
+            {"id": "Mlle.Baptistine", "group": 1},
+        ],
+        "links": [
+            {"source": "Napoleon", "target": "Myriel", "value": 1},
+            {"source": "Mlle.Baptistine", "target": "Myriel", "value": 8},
+        ],
+    }
+
+    return sample_json
+
+
+@router.get("/memgraph/single", response_model=schemas.GraphData)
+def sample_graph() -> Any:
+    """
+    Retrieve search subject and nearest nodes 3D data.
+    """
+    sample_json = {
+        "nodes": [
+            {"id": "Myriel", "group": 1},
+            {"id": "Napoleon", "group": 1},
+            {"id": "Mlle.Baptistine", "group": 1},
+        ],
+        "links": [
+            {"source": "Napoleon", "target": "Myriel", "value": 1},
+            {"source": "Mlle.Baptistine", "target": "Myriel", "value": 8},
+        ],
+    }
+
+    return sample_json
+
+
+@router.get("/memgraph/double", response_model=schemas.GraphData)
+def sample_graph() -> Any:
+    """
+    Retrieve graph data that connects two search subjects.
+    """
+    sample_json = {
+        "nodes": [
+            {"id": "Myriel", "group": 1},
+            {"id": "Napoleon", "group": 1},
+            {"id": "Mlle.Baptistine", "group": 1},
+        ],
+        "links": [
+            {"source": "Napoleon", "target": "Myriel", "value": 1},
+            {"source": "Mlle.Baptistine", "target": "Myriel", "value": 8},
+        ],
+    }
+
+    return sample_json
+
+
+@router.get("/sankey", response_model=schemas.GraphData)
+def sample_graph() -> Any:
+    """
+    Retrieve sankey data.
+    """
+    sample_json = {
+        "nodes": [
+            {"id": "Myriel", "group": 1},
+            {"id": "Napoleon", "group": 1},
+            {"id": "Mlle.Baptistine", "group": 1},
+        ],
+        "links": [
+            {"source": "Napoleon", "target": "Myriel", "value": 1},
+            {"source": "Mlle.Baptistine", "target": "Myriel", "value": 8},
+        ],
+    }
+
+    return sample_json
+
+
+@router.get("/search_terms", response_model=List[schemas.GraphSearch])
+def sample_graph() -> Any:
+    """
+    Retrieve search terms.
+    """
+    search_terms = ["thing1", "thing2", "thing3"]
+
+    return search_terms
