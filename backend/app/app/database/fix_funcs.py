66c65610086b94872b54badc386adfd1f93dff94
phaedrat <phaedratarrant@gmail.com>
2023-01-14 15:24:24 -0700

    Code for migrating party orgs to preferred party name/id

---
+++ backend/app/app/database/fix_funcs.py
@@ -0,0 +1,116 @@
+from schema_creation.sqlmodel_build import OrganizationMembership, Organization, \
+    CommunicationsOrgOrg, CommunicationsPersonOrg, OrganizationType, FundingPersonOrg, Funding
+from sqlmodel import select
+import common_func as cf
+
+
+def return_preferred_party_name(match_name_in_question):
+    bloc_name_set = {"blocquebecois", "blocquebecoiscaucus"}
+    ndp_name_set = {"newdemocraticparty", "ndpcaucus", "ndp"}
+    liberal_name_set = {"liberalpartyofcanada", "liberalcaucus", "liberal", "partiliberal",
+                        "liberalpartyofcanadaofficialopposition"}
+    con_name_set = {"conservativepartyofcanada", "conservative", "conservativecaucus"}
+    ind_name_set = {"independent", "independentcaucus"}
+
+    if match_name_in_question in bloc_name_set:
+        return "blocquebecois"
+
+    if match_name_in_question in ndp_name_set:
+        return "newdemocraticparty"
+
+    if match_name_in_question in liberal_name_set:
+        return "liberalpartyofcanada"
+
+    if match_name_in_question in con_name_set:
+        return "conservativepartyofcanada"
+
+    if match_name_in_question in ind_name_set:
+        return "independent"
+
+    return -1
+
+
+def replace_organization(old_id, new_id, sess, actually_do_it):
+
+    # update communicationsorgorg party_1 links
+    sql_query = select(CommunicationsOrgOrg).where(CommunicationsOrgOrg.party_1 == old_id)
+    res = sess.exec(sql_query).all()
+
+    for each_commsorgorg_p1 in res:
+        each_commsorgorg_p1.party_1 = new_id
+
+        if actually_do_it:
+            sess.add(each_commsorgorg_p1)
+            sess.commit()
+
+    # update communicationsorgorg party_2 links
+    sql_query = select(CommunicationsOrgOrg).where(CommunicationsOrgOrg.party_2 == old_id)
+    res = sess.exec(sql_query).all()
+
+    for each_commsorgorg_p2 in res:
+        each_commsorgorg_p2.party_2 = new_id
+
+        if actually_do_it:
+            sess.add(each_commsorgorg_p2)
+            sess.commit()
+
+    # update communicationspersonorg links
+    sql_query = select(CommunicationsPersonOrg).where(CommunicationsPersonOrg.organization == old_id)
+    res = sess.exec(sql_query).all()
+
+    for each_commspersonorg in res:
+        each_commspersonorg.organization = new_id
+
+        if actually_do_it:
+            sess.add(each_commspersonorg)
+            sess.commit()
+
+    # update fundingpersonorg links
+    sql_query = select(FundingPersonOrg).where(FundingPersonOrg.organization == old_id)
+    res = sess.exec(sql_query).all()
+
+    for each_fundingpersonorg in res:
+        each_fundingpersonorg.organization = new_id
+
+        if actually_do_it:
+            sess.add(each_fundingpersonorg)
+            sess.commit()
+
+    # update funding party_1 links
+    sql_query = select(Funding).where(Funding.party_1 == old_id)
+    res = sess.exec(sql_query).all()
+
+    for each_funding_p1 in res:
+        each_funding_p1.party_1 = new_id
+
+        if actually_do_it:
+            sess.add(each_funding_p1)
+            sess.commit()
+
+    # update funding party_2 links
+    sql_query = select(Funding).where(Funding.party_2 == old_id)
+    res = sess.exec(sql_query).all()
+
+    for each_funding_p2 in res:
+        each_funding_p2.party_2 = new_id
+
+        if actually_do_it:
+            sess.add(each_funding_p2)
+            sess.commit()
+
+    # update organizationmembership links
+    sql_query = select(OrganizationMembership).where(OrganizationMembership.organization == old_id)
+    res = sess.exec(sql_query).all()
+
+    for each_membership in res:
+        each_membership.organization = new_id
+
+        if actually_do_it:
+            sess.add(each_membership)
+            sess.commit()
+
+    # delete organization entry
+    if actually_do_it:
+        final_moments = sess.exec(select(Organization).where(Organization.id == old_id)).first()
+        sess.delete(final_moments)
+        sess.commit()
---
+++ backend/app/app/database/fix_orgs_003.py
@@ -0,0 +1,57 @@
+from schema_creation.sqlmodel_build import Organization, \
+    OrganizationType
+from sqlmodel import select
+import common_func as cf
+import fix_funcs
+
+sess = cf.create_session(debug=True)
+
+actually_do_it = False
+
+sql_query = select(Organization)
+res_all_orgs = sess.exec(sql_query).all()
+
+for each_org in res_all_orgs:
+    name_in_question = each_org.match_name
+    preferred_match_name = fix_funcs.return_preferred_party_name(name_in_question)
+
+    if preferred_match_name == -1:
+        # then it's not a recognized party name
+        continue
+    else:
+        # recognized as a party name
+        sql_query = select(Organization).where(Organization.match_name == preferred_match_name)
+        keeper_org = sess.exec(sql_query).first()
+        keeper_id = keeper_org.id
+        print(f"we are going to keep {keeper_org.display_name} (id {keeper_id})")
+
+        # update keeper organization_type = political party
+        stat = select(OrganizationType.id).where(
+            OrganizationType.match_name == "politicalparty"
+        )
+        type_party_id = sess.exec(stat).first()
+
+        keeper_org.organization_type = type_party_id
+
+        # update keeper parent_organization = federalgovernmentofcanada
+        stat = select(Organization.id).where(
+            Organization.match_name == "federalgovernmentofcanada"
+        )
+        fed_id = sess.exec(stat).first()
+
+        keeper_org.parent_organization = fed_id
+
+        if actually_do_it:
+            sess.add(keeper_org)
+            sess.commit()
+
+        if name_in_question != preferred_match_name:
+            print(f"\tthe following organization should be eliminated: {name_in_question}")
+
+            fix_funcs.replace_organization(old_id=each_org.id,
+                                           new_id=keeper_id,
+                                           sess=sess,
+                                           actually_do_it=actually_do_it)
+
+print("END")
+sess.close()
