eb62df82b4ab71f017d7fb5196206265dfa699c5
Ben Welwood <benwelwood@gmail.com>
2023-01-17 22:11:51 -0700

    remove items from postgres db to troubleshoot

--- backend/app/app/api/deps.py
+++ backend/app/app/api/deps.py
@@ -1,10 +1,10 @@
-from typing import Generator
+# from typing import Generator
 
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
-from pydantic import ValidationError
-from sqlalchemy.orm import Session
+# from fastapi import Depends, HTTPException, status
+# from fastapi.security import OAuth2PasswordBearer
+# from jose import jwt
+# from pydantic import ValidationError
+# from sqlalchemy.orm import Session
 
-from app import crud, models, schemas
-from app.core import security
+# from app import crud, models, schemas
+# from app.core import security
--- backend/app/app/db/base.py
+++ backend/app/app/db/base.py
@@ -2,5 +2,6 @@
 # imported by Alembic
 from app.db.base_class import Base  # noqa
 from app.models.user import User  # noqa
-from app.models.item import Item  # noqa
+
+# from app.models.item import Item  # noqa
 from app.models.token import AccessToken  # noqa
