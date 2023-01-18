510a6c6088a195485546fc6e77879a19cdbc980d
Ben Welwood <benwelwood@gmail.com>
2023-01-17 22:14:09 -0700

    beginning of async search and graph retrieval from front end

--- frontend/src/api/graph.ts
+++ frontend/src/api/graph.ts
@@ -1,17 +1,34 @@
 import axios from './axios'
 
 interface Graph {
-    nodes: {
-      "id": string,
-      "name": string,
-      "person": boolean,
-      "db_id": string,
-    },
-    links: any[],
+  nodes: {
+    id: string
+    name: string
+    type: string
+    val: number
+  }
+  links: {
+    source: 'string'
+    target: 'string'
+    type: 'string'
+    color: 'string'
+    dash: [0]
+    amount: 0
+  }
 }
 
-export function getGraph() {
-  return axios.get<Graph[]>('/forcegraph/sample').then(res => res.data)
+interface SearchQuery {
+  query: string
 }
 
+export async function getGraph() {
+  return axios.get<Graph[]>('/forcegraph/sample').then((res) => res.data)
+}
 
+export async function getOptions() {
+  return axios.get<SearchQuery[]>('/forcegraph/search_options').then((res) => res.data)
+}
+
+export async function getSearch() {
+  return axios.get<SearchQuery[]>('/forcegraph/search').then((res) => res.data)
+}
