4c9e7364b3aae0e3295f4691ab0306e643f62c6c
Ben Welwood <benwelwood@gmail.com>
2023-01-19 13:45:50 -0700

    react-query wip

--- frontend/src/api/graph.ts
+++ frontend/src/api/graph.ts
@@ -21,14 +21,19 @@ interface SearchQuery {
   query: string
 }
 
-export async function getGraph() {
+export async function getSampleGraph() {
   return axios.get<Graph[]>('/forcegraph/sample').then((res) => res.data)
 }
 
-export async function getOptions() {
-  return axios.get<SearchQuery[]>('/forcegraph/search_options').then((res) => res.data)
-}
+// export async function getOptions({ }) {
+//   return axios.get<SearchQuery[]>('/forcegraph/search_options', 'alex').then((res) => res.data)
+// }
 
 export async function getSearch() {
-  return axios.get<SearchQuery[]>('/forcegraph/search').then((res) => res.data)
+  return axios.post<SearchQuery[]>('/forcegraph/search').then((res) => res.data)
 }
+
+export const getOptions = (query: string) => {  
+  return axios.post<SearchQuery[]>('/forcegraph/search_options?query', query).then((res) => res.data)
+}
+  // 'http://localhost/api/v1/forcegraph/search_options?query=alex'
