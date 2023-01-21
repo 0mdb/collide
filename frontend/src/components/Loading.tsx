7bef2ff35199a6423b5b5e0bcaff1ffba02db28a
Ben Welwood <benwelwood@gmail.com>
2023-01-21 16:19:07 -0700

    loading spinner wip

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -4,8 +4,10 @@ import React from 'react'
 
 function Loading() {
   return (
-    <div>
-      <span className='animate-pulse'>Loading...</span>
+    <div className='grid min-h-screen'>
+      <div className='flex items-center gap-2 text-gray-500'>
+        <span className='block h-16 w-16 animate-spin rounded-full border-4 border-t-primary'></span>
+      </div>
     </div>
   )
 }
