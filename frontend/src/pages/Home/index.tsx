801259f934aa46bda5121ae1d0a7c8fe329f027d
Ben Welwood <benwelwood@gmail.com>
2023-01-21 16:24:40 -0700

    fix Home deps

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,1 +1,6 @@
 export { default } from './Home'
+/* export { default } from './Home3' */
+
+export { default as Law } from './Law'
+export { default as Money } from './Money'
+export { default as UserSettings } from './UserSettings'
