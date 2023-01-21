9da26da7046398688736f067d1e52e28b6a617c5
Ben Welwood <benwelwood@gmail.com>
2023-01-21 16:18:08 -0700

    export loading and theme button properly

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -1,3 +1,6 @@
 export { default as Unauthorized } from './Unauthorized'
 export { default as RequireAuth } from './RequireAuth'
 export { default as PersistLogin } from './PersistLogin'
+
+export { default as Loading } from './Loading'
+export { default as ThemeButton } from './ThemeButton'
