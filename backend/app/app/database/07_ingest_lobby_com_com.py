89e970f776adf4fb96a13e053280484b458c9e1f
phaedrat <phaedratarrant@gmail.com>
2023-01-25 20:21:24 -0700

    Refactoring database ingestion files (8/12)

--- backend/app/app/database/07_ingest_lobby_com_com.py
+++ backend/app/app/database/07_ingest_lobby_com_com.py
@@ -1,170 +1,75 @@
-from datetime import datetime, date
-import os
-import pathlib
 import pandas as pd
 import numpy as np
 import glob
-from sqlmodel import Session, create_engine, select, engine
-from schema_creation.sqlmodel_build import (
-    Person, Source, CommsTopic, Communications,
-)
-from parse_injest.utils import create_match_name
-
-# Preamble, folder locations
-project_name = "app"  # collide\backend\app\app
-data_dir = "data"
-data_dir_com = "lobby_comms"
-
-# Folder creation, directories
-curr_dir_name = os.path.dirname(__file__)
-
-absolute_project_path = None
-for i in pathlib.Path(curr_dir_name).parents:
-    if i.name == project_name:
-        absolute_project_path = i.absolute()
-        break
-
-com_dir = os.path.join(absolute_project_path, data_dir, data_dir_com)
-
-dpoh_csv = glob.glob(com_dir + "/*DpohExport.csv")
-if len(dpoh_csv) > 1:
-    raise RuntimeError("More than one csv detected.")
-df_dpoh = pd.read_csv(dpoh_csv[0])
-
-prim_csv = glob.glob(com_dir + "/*PrimaryExport.csv")
-if len(prim_csv) > 1:
-    raise RuntimeError("More than one csv detected.")
-df_prim = pd.read_csv(prim_csv[0])
-
-topic_csv = glob.glob(com_dir + "/*SubjectMattersExport.csv")
-if len(topic_csv) > 1:
-    raise RuntimeError("More than one csv detected.")
-df_topic = pd.read_csv(topic_csv[0])
-
-df_dpoh = df_dpoh.merge(df_prim, on='COMLOG_ID', how='left')
-df_dpoh = df_dpoh.merge(df_topic, on='COMLOG_ID', how='left')
-df_dpoh["SUBJ_MATTER_OBJET"].replace("Other", np.NaN, inplace=True)
-df_dpoh["merged_topic"] = df_dpoh["SUBJ_MATTER_OBJET"].combine_first(df_dpoh["OTHER_SUBJ_MATTER_AUTRE_OBJET"])
-
-gov_first_name_lst = df_dpoh["DPOH_FIRST_NM_PRENOM_TCPD"].to_list()
-gov_last_name_lst = df_dpoh["DPOH_LAST_NM_TCPD"].to_list()
-corp_first_name_lst = df_dpoh["RGSTRNT_1ST_NM_PRENOM_DCLRNT"].to_list()
-corp_last_name_lst = df_dpoh["RGSTRNT_LAST_NM_DCLRNT"].to_list()
-topic_lst = df_dpoh["merged_topic"].to_list()
-date_lst = df_dpoh["COMM_DATE"].to_list()
+from DirectoryHandler import DirectoryHandler
+import common_func as cf
 
 
-# Dummy database information
-db_host = "localhost"
-db_name = "lq_test"
-db_user = "test_user"
-db_pw = "changethis"
-schema_name = "lf_mockup_2"
+def insert_lobbycomm_communications():
+    # TODO: Test on fresh db 0/1
+    # TODO: Test on populated db 1/1
 
-verbose = False
-motor = create_engine(
-    f"postgresql+psycopg2://{db_user}:{db_pw}@localhost/{db_name}", echo=verbose
-)
+    # Preamble, folder locations
+    dh_comms = DirectoryHandler("lobby_comms")
 
-session = Session(motor)
+    dpoh_csv = glob.glob(dh_comms.path_of_interest + "/*DpohExport.csv")
+    if len(dpoh_csv) > 1:
+        raise RuntimeError("More than one csv detected.")
+    df_dpoh = pd.read_csv(dpoh_csv[0])
 
-# Grab shared data source ID
-# TODO: Add remaining logic to get most recent
-stat = select(Source.id).where(
-    Source.data_source == "Office of the Commissioner of Lobbying of Canada, Monthly Communication Reports"
-)
-res_source = session.exec(stat).all()
+    prim_csv = glob.glob(dh_comms.path_of_interest + "/*PrimaryExport.csv")
+    if len(prim_csv) > 1:
+        raise RuntimeError("More than one csv detected.")
+    df_prim = pd.read_csv(prim_csv[0])
 
-if len(res_source) > 1:
-    raise RuntimeError("Too many sources identified")
+    topic_csv = glob.glob(dh_comms.path_of_interest + "/*SubjectMattersExport.csv")
+    if len(topic_csv) > 1:
+        raise RuntimeError("More than one csv detected.")
+    df_topic = pd.read_csv(topic_csv[0])
 
-# TODO: Fix all this duplicated code :| (resolve with member xml .py as well)
-######################################################################################
-# Populate organizations of people in DPOH
-######################################################################################
+    # Combining three files into one
+    df_dpoh = df_dpoh.merge(df_prim, on='COMLOG_ID', how='left')
+    df_dpoh = df_dpoh.merge(df_topic, on='COMLOG_ID', how='left')
+    df_dpoh["SUBJ_MATTER_OBJET"].replace("Other", np.NaN, inplace=True)
+    df_dpoh["merged_topic"] = df_dpoh["SUBJ_MATTER_OBJET"].combine_first(df_dpoh["OTHER_SUBJ_MATTER_AUTRE_OBJET"])
 
-for gov_f, gov_l, corp_f, corp_l, topic, dt in zip(gov_first_name_lst, gov_last_name_lst,
-                                                   corp_first_name_lst, corp_last_name_lst,
-                                                   topic_lst, date_lst):
-    # if datetime.fromisoformat(dt).date() > date(2022, 6, 14):
-    #     continue
+    gov_first_name_lst = df_dpoh["DPOH_FIRST_NM_PRENOM_TCPD"].to_list()
+    gov_last_name_lst = df_dpoh["DPOH_LAST_NM_TCPD"].to_list()
+    corp_first_name_lst = df_dpoh["RGSTRNT_1ST_NM_PRENOM_DCLRNT"].to_list()
+    corp_last_name_lst = df_dpoh["RGSTRNT_LAST_NM_DCLRNT"].to_list()
+    topic_lst = df_dpoh["merged_topic"].to_list()
+    date_lst = df_dpoh["COMM_DATE"].to_list()
 
-    # Gov and corp names should already be populated in table
-    gov_party = f"{gov_f} {gov_l}"
-    gov_party = create_match_name(gov_party)
+    session = cf.create_session(debug=True)
 
-    corp_party = f"{corp_f} {corp_l}"
-    corp_party = create_match_name(corp_party)
+    # Grab sources (Monthly Communication Reports)
+    dh_comms.load_meta_file()
+    comms_source_objs = cf.add_sources(session, [{"data_source": dh_comms.source_name,
+                                                  "date_obtained": dh_comms.source_age,
+                                                  "misc_data": dh_comms.source_misc}])
+    comms_source_id = comms_source_objs[0].id
 
-    # Get party_1 ID
-    stat = select(Person.id).where(
-        Person.match_name == gov_party
-    )
-    res_gov = session.exec(stat).all()
+    # Populate organizations of people in DPOH
+    for gov_f, gov_l, corp_f, corp_l, topic, dt in zip(gov_first_name_lst, gov_last_name_lst,
+                                                       corp_first_name_lst, corp_last_name_lst,
+                                                       topic_lst, date_lst):
+        # if datetime.fromisoformat(dt).date() > date(2022, 6, 14):
+        #     continue
 
-    if len(res_gov) > 1:
-        raise RuntimeError("Too many people identified")
+        # Gov and corp names should already be populated in table
+        gov_party = f"{gov_f} {gov_l}"
+        gov_person_id = cf.get_person_id(session, gov_party)
 
-    # Get party_2 ID
-    stat = select(Person.id).where(
-        Person.match_name == corp_party
-    )
-    res_corp = session.exec(stat).all()
+        corp_party = f"{corp_f} {corp_l}"
+        corp_person_id = cf.get_person_id(session, corp_party)
 
-    if len(res_corp) > 1:
-        raise RuntimeError("Too many people identified")
+        # COMM TABLE
+        comms_obj = cf.add_communication(session, [{
+            "party_1": gov_person_id,
+            "party_2": corp_person_id,
+            "com_date": dt,
+            "topic": topic,
+            "source_id": comms_source_id
+        }])
 
-    if len(res_gov) == 0 or len(res_corp) == 0:
-        raise RuntimeError("Run prior step to ensure corp and gov parties are populated in person table")
-
-    # COMM TABLE
-    # check if entry unique
-    stat = select(Communications.id).where(
-        Communications.party_1 == res_gov[0]
-    ).where(
-        Communications.party_2 == res_corp[0]
-    ).where(
-        Communications.com_date == datetime.fromisoformat(dt).date()
-    )
-    res_1 = session.exec(stat).all()
-
-    if len(res_1) > 1:
-        raise RuntimeError("Too many communications identified")
-
-    stat = select(Communications.id).where(
-        Communications.party_1 == res_corp[0]
-    ).where(
-        Communications.party_2 == res_gov[0]
-    ).where(
-        Communications.com_date == datetime.fromisoformat(dt).date()
-    )
-    res_2 = session.exec(stat).all()
-
-    if len(res_2) > 1:
-        raise RuntimeError("Too many communications identified")
-
-    if len(res_1) == 0 and len(res_2) == 0:
-        # New communication
-        # Get topic ID
-        stat = select(CommsTopic.id).where(
-            CommsTopic.match_name == create_match_name(topic)
-        )
-        res_topic = session.exec(stat).all()
-
-        if len(res_topic) > 1:
-            raise RuntimeError("Too many topics identified")
-
-        if verbose:
-            print(f"TOPIC: {topic}")
-
-        ot = Communications(party_1=res_gov[0],
-                            party_2=res_corp[0],
-                            com_date=datetime.fromisoformat(dt),
-                            topic=res_topic[0],
-                            source=res_source[0]
-                            )
-
-        session.add(ot)
-        session.commit()
-
-session.close()
+    session.close()
