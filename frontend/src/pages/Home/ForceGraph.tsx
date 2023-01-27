000193aae378c986aecbb6a94a591cabadeab326
Ben Welwood <benwelwood@gmail.com>
2023-01-27 15:26:34 -0700

    disable refetch on focus

--- frontend/src/pages/Home/ForceGraph.tsx
+++ frontend/src/pages/Home/ForceGraph.tsx
@@ -60,6 +60,7 @@ function ForceGraph() {
   } = useQuery({
     queryKey: ['getSampleGraph'],
     queryFn: () => getSampleGraph(),
+    refetchOnWindowFocus: false,
   })
 
   const {
@@ -69,6 +70,7 @@ function ForceGraph() {
   } = useQuery({
     queryKey: ['getOptions'],
     queryFn: () => getOptions(''),
+    refetchOnWindowFocus: false,
   })
 
   // const {
