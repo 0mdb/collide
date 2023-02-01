1c754412a0e251cc9302ee378a96af24b6d414a9
Ben Welwood <benwelwood@gmail.com>
2023-01-31 20:34:55 -0700

    Add home2 for mobile layout testing

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 export { default } from './Home'
-/* export { default } from './Home3' */
 
 export { default as Law } from './Law'
+export { default as Home2 } from './Home2'
 export { default as Money } from './Money'
 export { default as UserSettings } from './UserSettings'
