bdd59992818634a900ba38a41cdf6eda6943c80d
Ben Welwood <benwelwood@gmail.com>
2023-02-03 20:36:01 -0700

    Centralize API calls

--- frontend/src/api/graph.ts
+++ frontend/src/api/graph.ts
@@ -22,16 +22,16 @@ interface SearchQuery {
 }
 
 export async function getSampleGraph() {
-  return axios.get<graphDataType[]>('/forcegraph/sample').then((res) => res.data)
+  return await axios.get<graphDataType>('/forcegraph/sample').then((res) => res.data)
 }
 
 export async function getOptions(query: string) {
   const SEARCH_URL = 'forcegraph/search_options?query='
   const match_name = query.toLowerCase().split(' ').join('')
-  return axios.post(SEARCH_URL + match_name).then((res) => res.data)
+  return await axios.post(SEARCH_URL + match_name).then((res) => res.data)
 }
 
 export async function getGraph(query: string) {
-console.log('getGraph', query)
-return axios.post('forcegraph/search/' + query).then((res) => res.data)
+  console.log('getGraph', query)
+  return await axios.post('forcegraph/search/' + query).then((res) => res.data)
 }
