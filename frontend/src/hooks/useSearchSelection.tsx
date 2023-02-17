3ae117437234bb399cd07b19559b1abfea9be2bd
Ben Welwood <benwelwood@gmail.com>
2023-02-17 16:21:11 -0700

    add hook to track current selection

---
+++ frontend/src/hooks/useSearchSelection.tsx
@@ -0,0 +1,10 @@
+//function that keeps track of the state of the search bar selection. it returns seletedSearch and setSelectedSearch
+import React, { useState } from 'react'
+
+function useSearchSelection() {
+  const [selectedSearch, setSelectedSearch] = useState('bradleygrant')
+  console.log('selected search set to ', selectedSearch)
+  return [selectedSearch, setSelectedSearch]
+}
+
+export default useSearchSelection
