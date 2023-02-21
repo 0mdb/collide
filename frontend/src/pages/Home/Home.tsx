562e42d04638f6ed0b106e959c117b985281565e
Ben Welwood <benwelwood@gmail.com>
2023-02-21 15:35:27 -0700

    hide fab button until after first search

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -201,20 +201,12 @@ export default function Home2() {
           </button>
           <div className='flex flex-1 justify-between px-4'>
             <div className='flex flex-1'>
-              <button
-                type='button'
-                className='relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                onClick={() => setOpen(true)}
-              >
-                {/* <SearchForm selected={selected} setSelected={setSelected} /> */}
-                <Search2
-                  open={open}
-                  setOpen={setOpen}
-                  selected={selected}
-                  setSelected={setSelected}
-                />
-              </button>
-              {/* <Search2 /> */}
+              <Search2
+                open={open}
+                setOpen={setOpen}
+                selected={selected}
+                setSelected={setSelected}
+              />
             </div>
             <div className='ml-4 flex items-center md:ml-6'>
               <button
@@ -269,12 +261,15 @@ export default function Home2() {
             <div className='py-4'>
               <div className='mx-auto my-auto h-full max-w-7xl px-4 sm:px-6 md:px-8'>
                 {/* Replace with your content */}
-                <FabButton open={open} setOpen={setOpen} />
                 {/* {selected} */}
                 {!!selected ? (
-                  <ForceGraph darkMode={darkMode} selected={selected} setSelected={setSelected} />
+                  <>
+                    <ForceGraph darkMode={darkMode} selected={selected} setSelected={setSelected} />
+
+                    <FabButton open={open} setOpen={setOpen} />
+                  </>
                 ) : (
-                  <EmptyGraph />
+                  <EmptyGraph open={open} setOpen={setOpen} />
                 )}
               </div>
             </div>
