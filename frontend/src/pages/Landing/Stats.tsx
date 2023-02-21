f0b95fb6c89f64129269cbbc407ca6684f3a3a36
phaedrat <phaedratarrant@gmail.com>
2023-02-20 21:27:16 -0700

    Issue #21. Importing strings. Working on colouring.

--- frontend/src/pages/Landing/Stats.tsx
+++ frontend/src/pages/Landing/Stats.tsx
@@ -1,32 +1,40 @@
+import { ProjectName, StatsPunchline, Stat1Label, Stat1Value, Stat2Label, Stat2Value, Stat3Label, Stat3Value  } from '../../assets/landing_strings.tsx'
+
 export default function Stats() {
   return (
-    <div className='bg-slate-300'>
+    <div className='bg-secondary'>
       <div className='mx-auto max-w-7xl py-12 px-6 sm:py-16 lg:px-8 lg:py-20'>
         <div className='mx-auto max-w-4xl text-center'>
-          <h2 className='text-3xl font-bold tracking-tight text-primary sm:text-4xl'>LobbyRadar</h2>
-          <p className='mt-3 text-xl text-gray-600 sm:mt-4'>
-            Just some wild people making politics more accessible
+          <h2 className='text-3xl font-bold tracking-tight text-primary-d dark:text-primary-d sm:text-4xl'>
+          <ProjectName />
+          </h2>
+          <p className='mt-3 text-xl text-secondary-text sm:mt-4'>
+            <StatsPunchline />
           </p>
         </div>
         <dl className='mt-10 text-center sm:mx-auto sm:grid sm:max-w-3xl sm:grid-cols-3 sm:gap-8'>
           <div className='flex flex-col'>
-            <dt className='order-2 mt-2 text-lg font-medium leading-6 text-black'>Lobbyists</dt>
-            <dd className='order-1 text-5xl font-bold tracking-tight dark:text-primary-d text-primary-l'>
-              178,647
-            </dd>
-          </div>
-          <div className='mt-10 flex flex-col sm:mt-0'>
-            <dt className='order-2 mt-2 text-lg font-medium leading-6 text-black'>
-              Tracked Donations
+            <dt className='order-2 mt-2 text-lg font-medium leading-6 text-secondary-text'>
+            <Stat1Label />
             </dt>
-            <dd className='order-1 text-5xl font-bold tracking-tight text-primary-l dark:text-primary-d'>
-              592,285,701
+            <dd className='order-1 text-5xl font-bold tracking-tight dark:text-primary-d text-primary-d'>
+            <Stat1Value />
             </dd>
           </div>
           <div className='mt-10 flex flex-col sm:mt-0'>
-            <dt className='order-2 mt-2 text-lg font-medium leading-6 text-black'>Legislation</dt>
-            <dd className='order-1 text-5xl font-bold tracking-tight text-primary-l dark:text-primary-d'>
-              6,761
+            <dt className='order-2 mt-2 text-lg font-medium leading-6 text-secondary-text'>
+            <Stat2Label />
+            </dt>
+            <dd className='order-1 text-5xl font-bold tracking-tight text-primary-d dark:text-primary-d'>
+            <Stat2Value />
+            </dd>
+          </div>
+          <div className='mt-10 flex flex-col sm:mt-0'>
+            <dt className='order-2 mt-2 text-lg font-medium leading-6 text-secondary-text'>
+            <Stat3Label />
+            </dt>
+            <dd className='order-1 text-5xl font-bold tracking-tight text-primary-d dark:text-primary-d'>
+            <Stat3Value />
             </dd>
           </div>
         </dl>
