39e2f0d95a5518823d2d8fdcb9b75154af9ce28b
Ben Welwood <benwelwood@gmail.com>
2023-02-21 14:53:59 -0700

    better styling on fab button

--- frontend/src/components/FabButton.tsx
+++ frontend/src/components/FabButton.tsx
@@ -7,7 +7,7 @@ function FabButton(props) {
     <div className='fixed bottom-0 right-0 mb-4 mr-4'>
       <button
         type='button'
-        className='fixed z-90 bottom-10 right-8 bg-blue-600 w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl'
+        className='fixed z-90 bottom-10 border border-transparent right-8 bg-primary shadow-sm w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
         onClick={() => props.setOpen(true)}
       >
         <Icon icon='search' size={20} className='fill-white' />
