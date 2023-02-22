e2c1ec12815e42df821cc8b36e5462d70e801c50
Ben Welwood <benwelwood@gmail.com>
2023-02-22 11:18:30 -0700

    call to action page for #30 needs copy

---
+++ frontend/src/pages/Landing/APINotify.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Footer from './Footer'
+import Navbar from './Nav'
+
+const APINotify = () => {
+  return (
+    <div className='w-full py-16 px-4'>
+      <div className='mx-auto grid max-w-[1240px] lg:grid-cols-3'>
+        <div className='my-4 lg:col-span-2'>
+          <h1 className='py-2 text-2xl font-bold sm:text-3xl md:text-4xl'>
+            Want tips & tricks to optimize your flow?
+          </h1>
+          <p>Sign up to our newsletter and stay up to date.</p>
+        </div>
+        <div className='my-4'>
+          <div className='flex w-full flex-col items-center justify-between sm:flex-row'>
+            <input
+              // className='flex w-full rounded-md p-3 text-black'
+              className='flex w-full rounded-md bg-gray-400 p-3 text-gray-500 placeholder-gray-500 dark:bg-gray-600 dark:text-gray-400'
+              type='email'
+              placeholder='Enter Email'
+            />
+            <button className='my-6 ml-4 w-[200px] rounded-md bg-[#00df9a] px-6 py-3 font-medium text-black'>
+              Notify Me
+            </button>
+          </div>
+          <p>
+            We care bout the protection of your data. Read our{' '}
+            <span className='text-[#00df9a]'>Privacy Policy.</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function APILanding() {
+  return (
+    <div>
+      <Navbar />
+      <APINotify />
+      <Footer />
+    </div>
+  )
+}
+
+export default APILanding
--- frontend/src/pages/Landing/Footer.tsx
+++ frontend/src/pages/Landing/Footer.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import {
-  FaDribbbleSquare,
-  FaFacebookSquare,
-  FaGithubSquare,
-  FaInstagram,
-  FaTwitterSquare,
-} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import { FaFacebookSquare, FaGithubSquare, FaInstagram, FaTwitterSquare } from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -21,24 +16,29 @@ const Footer = () => {
           <FaInstagram size={30} />
           <FaTwitterSquare size={30} />
           <FaGithubSquare size={30} />
-          <FaDribbbleSquare size={30} />
         </div>
       </div>
       <div className='mt-6 flex justify-between lg:col-span-2'>
         <div>
           <h6 className='font-medium'>Solutions</h6>
           <ul>
-            <li className='py-2 text-sm'>API</li>
-            <li className='py-2 text-sm'>Consulting</li>
+            <Link to='/'>
+              <li className='py-2'>Home</li>
+            </Link>
+            <Link to='/apinotify'>
+              <li className='py-2 text-sm'>API</li>
+            </Link>
           </ul>
         </div>
         <div>
           <h6 className='font-medium'>Support</h6>
           <ul>
-            <li className='py-2 text-sm'>Pricing</li>
-            <li className='py-2 text-sm'>Documentation</li>
-            <li className='py-2 text-sm'>Guides</li>
-            <li className='py-2 text-sm'>API Status</li>
+            <Link to='/faq'>
+              <li className='py-2 text-sm'>FAQ</li>
+            </Link>
+            <Link to='/contact'>
+              <li className='py-2 text-sm'>Contact</li>
+            </Link>
           </ul>
         </div>
         <div>
@@ -46,17 +46,14 @@ const Footer = () => {
           <ul>
             <li className='py-2 text-sm'>About</li>
             <li className='py-2 text-sm'>Blog</li>
-            <li className='py-2 text-sm'>Jobs</li>
-            {/* <li className='py-2 text-sm'>Press</li> */}
-            <li className='py-2 text-sm'>Join the Team</li>
           </ul>
         </div>
         <div>
           <h6 className='font-medium'>Legal</h6>
           <ul>
-            <li className='py-2 text-sm'>Claim</li>
-            <li className='py-2 text-sm'>Policy</li>
-            <li className='py-2 text-sm'>Terms</li>
+            <Link to='/legal'>
+              <li className='py-2 text-sm'>Policy</li>
+            </Link>
           </ul>
         </div>
       </div>
