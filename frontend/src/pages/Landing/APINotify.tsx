f144db8ec9ae509267da96f764a72e6d146e596e
Ben Welwood <benwelwood@gmail.com>
2023-02-22 16:11:23 -0700

    Sign up for waiting list.  Need API endpoint and email conf #30

--- frontend/src/pages/Landing/APINotify.tsx
+++ frontend/src/pages/Landing/APINotify.tsx
@@ -1,34 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Footer from './Footer'
 import Navbar from './Nav'
 
 const APINotify = () => {
   return (
     <div className='w-full py-16 px-4'>
-      <div className='mx-auto grid max-w-[1240px] lg:grid-cols-3'>
-        <div className='my-4 lg:col-span-2'>
-          <h1 className='py-2 text-2xl font-bold sm:text-3xl md:text-4xl'>
-            Want tips & tricks to optimize your flow?
-          </h1>
-          <p>Sign up to our newsletter and stay up to date.</p>
+      <div className='mx-auto max-w-[800px]'>
+        <h1 className='py-2 text-2xl font-bold sm:text-3xl md:text-4xl text-center'>
+          Get early access to the LobbyRadar API.
+        </h1>
+        <p className='text-center'>
+          Sign up to our waiting list and be the first to know when it's ready.
+        </p>
+        <div className='my-8 flex flex-col items-center justify-between sm:flex-row'>
+          <input
+            className='flex w-full rounded-md bg-gray-400 p-3 text-gray-500 placeholder-gray-500 dark:bg-gray-600 dark:text-gray-400'
+            type='email'
+            placeholder='Enter Email'
+          />
+          <button className='my-6 ml-4 w-[250px] rounded-md bg-primary dark:bg-primary-d px-6 py-3 font-medium text-morp'>
+            Join the waiting list
+          </button>
         </div>
-        <div className='my-4'>
-          <div className='flex w-full flex-col items-center justify-between sm:flex-row'>
-            <input
-              // className='flex w-full rounded-md p-3 text-black'
-              className='flex w-full rounded-md bg-gray-400 p-3 text-gray-500 placeholder-gray-500 dark:bg-gray-600 dark:text-gray-400'
-              type='email'
-              placeholder='Enter Email'
-            />
-            <button className='my-6 ml-4 w-[200px] rounded-md bg-[#00df9a] px-6 py-3 font-medium text-black'>
-              Notify Me
-            </button>
-          </div>
-          <p>
-            We care bout the protection of your data. Read our{' '}
-            <span className='text-[#00df9a]'>Privacy Policy.</span>
+        <Link to='/legal'>
+          <p className='text-center'>
+            By signing up, you agree to our <span className='text-muted'>Privacy Policy</span>.
           </p>
-        </div>
+        </Link>
       </div>
     </div>
   )
