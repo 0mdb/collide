8ffc71dd6016056bbd8eefa254efcc58bda6d68d
phaedrat <phaedratarrant@gmail.com>
2023-02-23 21:56:35 -0700

    Issue #38. Adding license skeletons. Forcegraph done.

---
+++ frontend/src/assets/license_privacy.tsx
@@ -0,0 +1,10 @@
+export const PrivacyLicense = () => {
+  return (
+  <div>
+    <h2>Privacy Policy</h2>
+      <p>
+        Coming soon...
+      </p>
+  </div>
+  )
+}
---
+++ frontend/src/assets/license_terms.tsx
@@ -0,0 +1,10 @@
+export const TermsLicense = () => {
+  return (
+  <div>
+    <h2>Terms of Use</h2>
+      <p>
+        Coming soon...
+      </p>
+  </div>
+  )
+}
