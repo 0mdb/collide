deeeb8b3a09da922bae90bbfd7743843a2479cc0
Ben Welwood <benwelwood@gmail.com>
2023-02-25 16:38:44 -0700

    Add back Items endpoints to adapt to user settings. #52

--- backend/app/app/schemas/user.py
+++ backend/app/app/schemas/user.py
@@ -5,14 +5,18 @@ from fastapi_users import schemas
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
     pass
+    # items: list[str] = []
 
 
 class UserCreate(schemas.BaseUserCreate):
     pass
 
+    # items: list[str] = []
+
 
 class UserUpdate(schemas.BaseUserUpdate):
     pass
+    # items: list[str] = []
 
 
 # Additional properties to return via API
