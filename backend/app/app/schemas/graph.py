735a0c3468372cd956c04fbbe9551dede4013922
jrodway <jrodway@ualberta.ca>
2023-02-27 07:28:10 -0700

    getting person/org/bill queries to happen from the search box

--- backend/app/app/schemas/graph.py
+++ backend/app/app/schemas/graph.py
@@ -1,31 +1,37 @@
 from __future__ import annotations
 
-from typing import List
+from typing import List, Optional
 
 from pydantic import BaseModel
 
 
 class Node(BaseModel):
-    id: str
-    name: str
-    type: str
-    val: int
+    id: Optional[str]
+    name: Optional[str]
+    type: Optional[str]
+    nodeColor: Optional[str]
+    value: Optional[int]
 
 
 class Link(BaseModel):
-    source: str
-    target: str
-    type: str
-    color: str
-    dash: List[int]
-    amount: int
+    source: Optional[str]
+    target: Optional[str]
+    type: Optional[str]
+    linkColor: Optional[str]
+    linkDirectionalArrowLength: Optional[int]
+    linkDirectionalArrowRelPos: Optional[float]
+    linkWidth: Optional[int]
+    dash: Optional[List[int]]
+    amount: Optional[int]
 
 
 class GraphData(BaseModel):
-    nodes: List[Node]
-    links: List[Link]
+    nodes: Optional[List[Node]]
+    links: Optional[List[Link]]
 
 
 class GraphSearchOptions(BaseModel):
     value: str
     label: str
+    type: str
+    bill_match: str
