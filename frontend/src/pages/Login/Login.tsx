60e8dcbe55c02066c6471f88bd026cf6ca304e79
phaedrat <phaedratarrant@gmail.com>
2023-02-26 22:23:00 -0700

    Issue #51. Centred. Unclear why SVG box is overflowing parent div.

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -61,18 +61,17 @@ const Login = () => {
 
   return (
     <>
-      <div className='flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8'>
-        <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-          <div className='w-12 h-12'>
+      <div className='flex min-h-full w-full flex-col justify-center items-center text-center py-12 sm:px-6 lg:px-8'>
+        <div className='mx-auto w-full'>
+          <div className='mx-auto w-14 h-14'>
            <IconSvg />
           </div>
-          <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
+          <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-secondary-l-text dark:text-secondary-d-text'>
             Sign in to your account
           </h2>
-          <p className='mt-2 text-center text-sm text-gray-600'>
-            Or{' '}
-            <Link to='/register' className='font-medium text-indigo-600 hover:text-indigo-500'>
-              make a free early access account
+          <p className='mt-2 text-center text-sm text-secondary-text dark:text-secondary-d-text'>
+            <Link to='/register' className='font-medium text-primary-d dark:text-primary-l hover:text-primary'>
+              Or make a free early access account
             </Link>
           </p>
         </div>
