5b25d4200bd060bac2330bb18b3b55d6ca1ee9c2
Ben Welwood <benwelwood@gmail.com>
2023-02-27 20:36:01 -0700

    Use local storage hook for darkmode setting #52

--- frontend/src/hooks/useDarkMode.tsx
+++ frontend/src/hooks/useDarkMode.tsx
@@ -1,30 +1,5 @@
 import { useEffect, useState } from 'react'
-import { useMediaQuery } from 'react-responsive'
-
-const useLocalStorage = (key, initialValue) => {
-  const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key)
-      return item ? JSON.parse(item) : initialValue
-    } catch (error) {
-      console.log(error)
-      return initialValue
-    }
-  })
-
-  const setValue = (value) => {
-    try {
-      const valueToStore = value instanceof Function ? value(storedValue) : value
-
-      setStoredValue(valueToStore)
-
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  return [storedValue, setValue]
-}
+import useLocalStorage from '../hooks/useLocalStorage'
 
 const useDarkMode = () => {
   const [enabled, setEnabled] = useLocalStorage('dark-theme')
@@ -36,7 +11,6 @@ const useDarkMode = () => {
 
     isEnabled ? bodyClass.add(className) : bodyClass.remove(className)
   }, [enabled, isEnabled])
-
   return [enabled, setEnabled]
 }
 
