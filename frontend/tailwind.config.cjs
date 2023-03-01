f783c1650a18087ef4208382b9f96df7537bcaae
phaedrat <phaedratarrant@gmail.com>
2023-02-28 22:13:43 -0700

    Issue #58. Adding accents and hovers to secondary colours

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -26,18 +26,39 @@ module.exports = {
         },
         'morp': '#f6f7f9',  // nearly white
         'muted': '#abb3bf',  // light gray
+
         'primary': '#4338ca',  // indigo-700
         'primary-l': '#818cf8',  // indigo-400
         'primary-d': '#312e81',  // indigo-900
+
         'secondary': '#64748b',  // slate-500
         'secondary-l': '#cbd5e1',  // slate-300
         'secondary-d': '#334155',  // slate-700
+
         'primary-text': '#ffffff',  // white
-        'secondary-text': '#ffffff',  // white
-        'primary-d-text': '#ffffff',  // white
-        'secondary-d-text': '#ffffff',  // white
         'primary-l-text': '#000000',  // black
+        'primary-d-text': '#ffffff',  // white
+
+//         'primary-text-accent':
+//         'primary-l-text-accent':
+//         'primary-d-text-accent':
+//
+//         'primary-text-accent-hov':
+//         'primary-l-text-accent-hov':
+//         'primary-d-text-accent-hov':
+
+        'secondary-text': '#ffffff',  // white
         'secondary-l-text': '#000000',  // black
+        'secondary-d-text': '#ffffff',  // white
+
+        'secondary-accent': '#4338ca',  // indigo-700
+        'secondary-l-accent': '#312e81',  // indigo-900
+        'secondary-d-accent': '#818cf8',  // indigo-400
+
+        'secondary-accent-hov': '#818cf8',  // indigo-400
+        'secondary-l-accent-hov': '#6366f1'  // indigo-500
+        'secondary-d-accent-hov': '#312e81',  // indigo-900
+
         'analogous1': '#8d38ca',  // magenta
         'analogous2': '#38b4ca',  // teal
       },
