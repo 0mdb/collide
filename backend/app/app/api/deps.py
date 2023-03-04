39b6a98c51d93ff3a44d2e02704abbbc0a764806
Ben Welwood <benwelwood@gmail.com>
2023-03-04 15:06:08 -0700

    disable items dependencies for now #52

--- backend/app/app/api/deps.py
+++ backend/app/app/api/deps.py
@@ -4,7 +4,8 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt
 from pydantic import ValidationError
-from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
 
 from app import crud, models, schemas
 from app.core import security
@@ -13,17 +14,21 @@ from app.core.users import current_active_superuser, current_active_user
 
 # from app.db.session import SessionLocal
 
+uri = "postgresql://postgres:changethis@db:5432/app"
+engine = create_engine(uri, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 reusable_oauth2 = OAuth2PasswordBearer(
     tokenUrl=f"{settings.API_V1_STR}/login/access-token"
 )
 
 
-def get_db() -> Generator:
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
+# def get_db() -> Generator:
+#     try:
+#         db = SessionLocal()
+#         yield db
+#     finally:
+#         db.close()
 
 
 
