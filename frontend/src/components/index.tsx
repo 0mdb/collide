7a049e0cc56d790e5f6ea8602ff33eeffa30cd78
Ben Welwood <benwelwood@gmail.com>
2023-03-10 11:52:16 -0700

    remove unused imports #66

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -1,7 +1,3 @@
-export { default as Unauthorized } from './Unauthorized'
-export { default as RequireAuth } from './RequireAuth'
-export { default as PersistLogin } from './PersistLogin'
-
 export { default as Loading } from './Loading'
 export { default as ThemeButton } from './ThemeButton'
 export { default as PageNotFound } from './PageNotFound'
