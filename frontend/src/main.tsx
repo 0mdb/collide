7418cf4a38c29677b54c975afca3817b0424f895
Ben Welwood <benwelwood@gmail.com>
2023-03-10 11:51:14 -0700

    wrap entire app with AuthProvider #52

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,22 +3,27 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import { AuthProvider } from './context/AuthProvider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import ScrollToTop from './components/ScrollToTop'
+import { AuthProvider } from 'react-auth-kit'
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
-  <BrowserRouter>
-    <AuthProvider>
+
+  <AuthProvider authType='localstorage' authName='_auth'>
+    <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <Routes>
-          <Route path='/*' element={<App />} />
-        </Routes>
-        <ReactQueryDevtools />
+        <ScrollToTop>
+          <Routes>
+            <Route path='/*' element={<App />} />
+          </Routes>
+          <ReactQueryDevtools />
+        </ScrollToTop>
       </QueryClientProvider>
-    </AuthProvider>
-  </BrowserRouter>,
+    </BrowserRouter>
+  </AuthProvider>,
   // </React.StrictMode>,
 )
