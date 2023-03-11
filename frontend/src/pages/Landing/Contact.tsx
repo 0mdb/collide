11bbe478cd2532cb28eae9f1ae45c941dc06b1ef
phaedrat <phaedratarrant@gmail.com>
2023-03-10 21:28:03 -0700

    Issue #71. Consistent styling.

--- frontend/src/pages/Landing/Contact.tsx
+++ frontend/src/pages/Landing/Contact.tsx
@@ -13,10 +13,10 @@ function ContactLanding() {
   const [agreed, setAgreed] = useState(false)
 
   return (
-    <div className='isolate bg-white py-16 px-6 sm:py-24 lg:px-8'>
+    <div className='isolate bg-secondary-l dark:bg-secondary text-secondary-l-text dark:text-secondary-text py-16 px-6 sm:py-24 lg:px-8'>
       <div className='mx-auto max-w-2xl text-center'>
-        <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>Contact us</h2>
-        <p className='mt-2 text-lg leading-8 text-gray-600'>
+        <h2 className='text-3xl font-bold tracking-tight sm:text-4xl'>Contact us</h2>
+        <p className='mt-2 text-lg leading-8 text-gray-600 dark:text-gray-200'>
           We're here to help. Send us a message and we'll respond as soon as possible.
         </p>
       </div>
@@ -25,7 +25,7 @@ function ContactLanding() {
           <div>
             <label
               htmlFor='first-name'
-              className='block text-sm font-semibold leading-6 text-gray-900'
+              className='block text-sm font-semibold leading-6'
             >
               First name
             </label>
@@ -35,14 +35,19 @@ function ContactLanding() {
                 name='first-name'
                 id='first-name'
                 autoComplete='given-name'
-                className='block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+                className='block w-full rounded-md
+                border border-secondary dark:border-secondary-d
+                text-secondary-l-text dark:text-secondary-l-text
+                py-2 px-3.5 placeholder-secondary shadow-sm text-sm leading-6
+                ring-1 ring-inset ring-gray-300
+                focus:border-primary focus:outline-none focus:ring-primary'
               />
             </div>
           </div>
           <div>
             <label
               htmlFor='last-name'
-              className='block text-sm font-semibold leading-6 text-gray-900'
+              className='block text-sm font-semibold leading-6'
             >
               Last name
             </label>
@@ -52,14 +57,19 @@ function ContactLanding() {
                 name='last-name'
                 id='last-name'
                 autoComplete='family-name'
-                className='block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+                className='block w-full rounded-md
+                border border-secondary dark:border-secondary-d
+                text-secondary-l-text dark:text-secondary-l-text
+                py-2 px-3.5 placeholder-secondary shadow-sm text-sm leading-6
+                ring-1 ring-inset ring-gray-300
+                focus:border-primary focus:outline-none focus:ring-primary'
               />
             </div>
           </div>
           <div className='sm:col-span-2'>
             <label
               htmlFor='company'
-              className='block text-sm font-semibold leading-6 text-gray-900'
+              className='block text-sm font-semibold leading-6'
             >
               Company
             </label>
@@ -69,12 +79,17 @@ function ContactLanding() {
                 name='company'
                 id='company'
                 autoComplete='organization'
-                className='block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+                className='block w-full rounded-md
+                border border-secondary dark:border-secondary-d
+                text-secondary-l-text dark:text-secondary-l-text
+                py-2 px-3.5 placeholder-secondary shadow-sm text-sm leading-6
+                ring-1 ring-inset ring-gray-300
+                focus:border-primary focus:outline-none focus:ring-primary'
               />
             </div>
           </div>
           <div className='sm:col-span-2'>
-            <label htmlFor='email' className='block text-sm font-semibold leading-6 text-gray-900'>
+            <label htmlFor='email' className='block text-sm font-semibold leading-6'>
               Email
             </label>
             <div className='mt-2.5'>
@@ -83,14 +98,19 @@ function ContactLanding() {
                 name='email'
                 id='email'
                 autoComplete='email'
-                className='block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+                className='block w-full rounded-md
+                border border-secondary dark:border-secondary-d
+                text-secondary-l-text dark:text-secondary-l-text
+                py-2 px-3.5 placeholder-secondary shadow-sm text-sm leading-6
+                ring-1 ring-inset ring-gray-300
+                focus:border-primary focus:outline-none focus:ring-primary'
               />
             </div>
           </div>
           <div className='sm:col-span-2'>
             <label
               htmlFor='phone-number'
-              className='block text-sm font-semibold leading-6 text-gray-900'
+              className='block text-sm font-semibold leading-6'
             >
               Phone number
             </label>
@@ -118,14 +138,19 @@ function ContactLanding() {
                 name='phone-number'
                 id='phone-number'
                 autoComplete='tel'
-                className='block w-full rounded-md border-0 py-2 px-3.5 pl-20 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+                className='block w-full rounded-md pl-20
+                border border-secondary dark:border-secondary-d
+                text-secondary-l-text dark:text-secondary-l-text
+                py-2 px-3.5 placeholder-secondary shadow-sm text-sm leading-6
+                ring-1 ring-inset ring-gray-300
+                focus:border-primary focus:outline-none focus:ring-primary'
               />
             </div>
           </div>
           <div className='sm:col-span-2'>
             <label
               htmlFor='message'
-              className='block text-sm font-semibold leading-6 text-gray-900'
+              className='block text-sm font-semibold leading-6'
             >
               Message
             </label>
@@ -134,7 +159,12 @@ function ContactLanding() {
                 name='message'
                 id='message'
                 rows={4}
-                className='block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+                className='block w-full rounded-md
+                border border-secondary dark:border-secondary-d
+                text-secondary-l-text dark:text-secondary-l-text
+                py-2 px-3.5 placeholder-secondary shadow-sm text-sm leading-6
+                ring-1 ring-inset ring-gray-300
+                focus:border-primary focus:outline-none focus:ring-primary'
                 defaultValue={''}
               />
             </div>
@@ -159,9 +189,9 @@ function ContactLanding() {
                 />
               </Switch>
             </div>
-            <Switch.Label className='text-sm leading-6 text-gray-600'>
+            <Switch.Label className='text-sm leading-6 text-secondary-l-text-accent dark:text-secondary-text-accent'>
               By selecting this, you agree to our{' '}
-              <Link to='/privacypolicy' className='font-semibold text-indigo-600'>
+              <Link to='/privacypolicy' className='font-semibold'>
                 privacy&nbsp;policy
               </Link>
               .
@@ -171,7 +201,16 @@ function ContactLanding() {
         <div className='mt-10'>
           <button
             type='submit'
-            className='block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+            className='block w-full rounded-md
+            border border-primary
+            bg-primary
+            px-3.5 py-2.5 text-center text-sm
+            font-medium text-secondary-d-text
+            shadow-sm
+            hover:border-primary-d hover:bg-primary-d hover:text-secondary-d-text hover:shadow-md
+            dark:border-primary-d dark:bg-primary-d dark:text-secondary-d-text
+            dark:hover:border-primary dark:hover:bg-primary
+            focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
           >
             Let's talk
           </button>
