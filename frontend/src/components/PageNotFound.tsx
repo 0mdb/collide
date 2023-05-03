2fe23c8baf3efe7994762b882e471ba75afba583
phaedrat <phaedratarrant@gmail.com>
2023-05-03 15:44:35 -0600

    Issue #73. Hid social media icons. Linked PageNotFound to contact form.

--- frontend/src/components/PageNotFound.tsx
+++ frontend/src/components/PageNotFound.tsx
@@ -1,4 +1,5 @@
 import { IconSvg } from './IconSvg'
+import { Link } from 'react-router-dom'
 
 export default function PageNotFound() {
   return (
@@ -31,27 +32,31 @@ export default function PageNotFound() {
                 Sorry, we couldn’t find the page you’re looking for.
               </p>
               <div className='mt-6'>
-                <a href='/' className='text-base font-medium text-indigo-600 hover:text-indigo-500'>
+              <Link to='/'>
+                <div className='text-base font-medium text-indigo-600 hover:text-indigo-500'>
                   Go back home
                   <span aria-hidden='true'> &rarr;</span>
-                </a>
+                </div>
+               </Link>
               </div>
             </div>
           </div>
         </main>
         <footer className='mx-auto w-full max-w-7xl flex-shrink-0 px-6 lg:px-8'>
           <nav className='flex justify-center space-x-4'>
-            <a href='#' className='text-sm font-medium text-gray-500 hover:text-gray-600'>
+            <Link to='/contact'>
+            <div className='text-sm font-medium text-gray-500 hover:text-gray-600'>
               Contact Support
-            </a>
-            <span className='inline-block border-l border-gray-300' aria-hidden='true' />
-            <a href='#' className='text-sm font-medium text-gray-500 hover:text-gray-600'>
-              Status
-            </a>
-            <span className='inline-block border-l border-gray-300' aria-hidden='true' />
-            <a href='#' className='text-sm font-medium text-gray-500 hover:text-gray-600'>
-              Twitter
-            </a>
+            </div>
+            </Link>
+{/*             <span className='inline-block border-l border-gray-300' aria-hidden='true' /> */}
+{/*             <a href='#' className='text-sm font-medium text-gray-500 hover:text-gray-600'> */}
+{/*               Status */}
+{/*             </a> */}
+{/*             <span className='inline-block border-l border-gray-300' aria-hidden='true' /> */}
+{/*             <a href='#' className='text-sm font-medium text-gray-500 hover:text-gray-600'> */}
+{/*               Twitter */}
+{/*             </a> */}
           </nav>
         </footer>
       </div>
